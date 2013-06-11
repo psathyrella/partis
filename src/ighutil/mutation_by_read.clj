d98afc7593ac645a30d4abe7b9425ccf28d0cab9
Connor McCoy <cmccoy@fhcrc.org>
2013-06-11 15:19:53 -0700

    Tweaks, now with J

--- src/ighutil/mutation_by_read.clj
+++ src/ighutil/mutation_by_read.clj
@@ -27,7 +27,15 @@
                  (inc ri)))
         res))))
 
-(defn identify-mutations-in-read [^SAMRecord read reference-bases]
+
+
+(defn- strip-allele [^String s]
+  (let [idx (.lastIndexOf s 42)]
+    (if (< idx 0)
+      s
+      (.substring s 0 idx))))
+
+(defn- identify-mutations-in-read [^SAMRecord read reference-bases]
   (let [aligned-blocks (.getAlignmentBlocks read)
         s (.getReadBases read)]
     (into [] (mapcat
@@ -39,27 +47,44 @@
    (fn [^String contig]
      (.getBases (.getSequence f contig)))))
 
+(defn- non-primary [^SAMRecord r]
+  (or (.getReadUnmappedFlag r) (.getNotPrimaryAlignmentFlag r)))
+
 (defn- identify-mutations-in-sam [^SAMFileReader sam ref-get]
   (let [sam-records (-> sam (.iterator) iterator-seq)]
-    (for [^SAMRecord r (remove (fn [^SAMRecord r]
-                                 (or (.getReadUnmappedFlag r)
-                                     (.getNotPrimaryAlignmentFlag r)))
-                               sam-records)]
+    (for [^SAMRecord r (remove non-primary sam-records)]
       (let [ref-bases (ref-get (.getReferenceName r))]
-        (identify-mutations-in-read r ref-bases)))))
+        {:name (.getReadName r)
+         :reference (strip-allele (.getReferenceName r))
+         :mutations (identify-mutations-in-read r ref-bases)}))))
+
+(defn- reference-per-read [^SAMFileReader sam]
+  (let [sam-records (-> sam (.iterator) iterator-seq)]
+    (into
+     {}
+     (for [^SAMRecord r (remove non-primary sam-records)]
+       [(.getReadName r) (strip-allele (.getReferenceName r))]))))
 
 (defcommand enumerate-mutations
-  "Enumerate mutations by sample"
+  "Enumerate mutations by V / J"
   {:opts-spec [["-j" "--jobs" "Number of processors" :default 1
                 :parse-fn #(Integer. ^String %)]]
-   :bind-args-to [reference sam-path]}
-  (time (let [ref (-> reference
-                 io/file
-                 ReferenceSequenceFileFactory/getReferenceSequenceFile)
-         ref-get (ref-getter ref)]
-     (with-open [sam (SAMFileReader. (io/file sam-path))]
-       (let [muts (identify-mutations-in-sam sam ref-get)]
-         (doall muts))))))
+   :bind-args-to [reference v-sam-path j-sam-path]}
+  (let [j-map (with-open [sam (SAMFileReader. (io/file j-sam-path))]
+                (reference-per-read sam))
+        ref (-> reference
+                io/file
+                ReferenceSequenceFileFactory/getReferenceSequenceFile)
+        ref-get (ref-getter ref)]
+    (println (count j-map) "reads")
+    (with-open [sam (SAMFileReader. (io/file v-sam-path))]
+      (let [muts (map
+                  (fn [{name :name :as m}] (assoc m :j-gene (get j-map name)))
+                  (identify-mutations-in-sam sam ref-get))]
+        (->> muts
+             (sort-by (juxt :reference :j-gene))
+             (take 5)
+             doall)))))
 
 (defn test-enumeration []
-  (enumerate-mutations ["ighv.fasta" "AJ_memory_001_v.bam"]))
+  (enumerate-mutations ["ighv.fasta" "AJ_memory_001_v.bam" "AJ_memory_001_j.bam"]))
