efe48d338e8c2652f2190659b3d06cf7daf6842e
Connor McCoy <cmccoy@fhcrc.org>
2013-06-11 17:58:20 -0700

    It makes a file!

--- src/ighutil/enumerate_mutations.clj
+++ src/ighutil/enumerate_mutations.clj
@@ -5,7 +5,9 @@
            [net.sf.samtools SAMRecord AlignmentBlock SAMFileReader])
   (:require [clojure.java.io :as io]
             [clojure.core.memoize :refer [memo-lu]]
+            [clojure.data.csv :as csv]
             [cliopatra.command :refer [defcommand]]
+            [ighutil.io :as zio]
             [ighutil.ubtree :as ub]))
 
 ;;; TODO: Better as for?
@@ -74,8 +76,11 @@
      (.getBases (.getSequence f contig)))))
 
 (defn summarize-mutation-partition [coll]
-  (let [sort-mutations (partial sort-by (juxt :ref-idx :ref :qry))
-        {j-gene :XJ cdr3-length :XL v-gene :reference} (first coll)]
+  (let [sort-mutations (comp vec (partial sort-by (juxt :ref-idx :ref :qry)))
+        {j-gene :XJ cdr3-length :XL v-gene :reference} (first coll)
+        coll (vec coll)
+        n-seqs (count coll)]
+    (println v-gene j-gene cdr3-length n-seqs)
     (loop [[r & rest] coll t ub/ubtree smap {} edges {}]
       (if r
         (let [{m' :mutations name :name} r
@@ -85,33 +90,41 @@
              rest
              t
              smap
-             (assoc edges name (get smap hit)))
+             (assoc edges name [(get smap hit) (- (count mutations) (count hit))]))
             (recur
              rest
              (ub/insert t mutations)
              (assoc smap mutations name)
              edges)))
-        {:v-gene v-gene :j-gene j-gene :cdr3-length cdr3-length :edges edges}))))
+        {:v-gene v-gene :j-gene j-gene :cdr3-length cdr3-length :edges edges
+         :n-seqs n-seqs}))))
+
+(defn- better-be-some [coll]
+  (assert (seq coll)))
 
 (defcommand enumerate-mutations
   "Enumerate mutations by V / J"
   {:opts-spec [["-j" "--jobs" "Number of processors" :default 1
-                :parse-fn #(Integer. ^String %)]]
+                :parse-fn #(Integer. ^String %)]
+               ["-o" "--out-file" "Destination path" :default System/out
+                :parse-fn zio/writer]]
    :bind-args-to [reference v-sam-path]}
   (let [ref (-> reference
                 io/file
                 ReferenceSequenceFileFactory/getReferenceSequenceFile)
         ref-get (ref-getter ref)]
     (with-open [sam (SAMFileReader. (io/file v-sam-path))]
-      (let [muts (identify-mutations-in-sam sam ref-get)]
-        (->> muts
-             (remove (comp zero? :n-mutations)) ;  Drop sequences without mutations from
-                                        ;  germline
-             (sort-by (juxt :reference :XJ :XL :n-mutations))
-             (partition-by (juxt :reference :XJ :XL))
-             (map summarize-mutation-partition)
-             (take 2)
-             doall)))))
-
-(defn test-enumeration []
-  (enumerate-mutations ["ighv.fasta" "AJ_memory_001_v.bam"]))
+      (let [muts (identify-mutations-in-sam sam ref-get)
+            summaries (->> muts
+                           ;; Drop sequences without mutations
+                           ;; from germline
+                           (remove (comp zero? :n-mutations))
+                           (sort-by (juxt :reference :XJ :XL :n-mutations))
+                           (partition-by (juxt :reference :XJ :XL))
+                           (map summarize-mutation-partition))]
+        (with-open [out-file out-file]
+          (csv/write-csv out-file [["v_gene" "j_gene" "n_seqs" "a" "b" "i"]])
+          (csv/write-csv out-file
+           (for [{v-gene :v-gene j-gene :j-gene edges :edges n-seqs :n-seqs} summaries
+                 [from [to i]] edges]
+             [v-gene j-gene n-seqs from to i])))))))
