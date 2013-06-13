87544b09f31c25d2816d01ebf741b54fdc1874db
Connor McCoy <cmccoy@fhcrc.org>
2013-06-13 15:08:23 -0700

    Fix some dependencies

--- project.clj
+++ project.clj
@@ -4,19 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 ;; GATK Requirements from bcbio.variation
-                 ;;[org.clojars.chapmanb/gatk-lite "2.5.2"]
                  [org.clojars.chapmanb/picard "1.90"]
                  [org.clojars.chapmanb/sam "1.90"]
-                 ;;[org.clojars.chapmanb/tribble "1.90"]
-                 ;;[org.clojars.chapmanb/variant "1.90"]
-                 ;;[org.simpleframework/simple-xml "2.0.4"]
-                 ;;[colt/colt "1.2.0"]
-                 ;;[it.unimi.dsi/fastutil "6.5.3"]
-                 ;;[log4j/log4j "1.2.17"]
-                 ;;[commons-lang/commons-lang "2.5"]
-                 ;;[org.apache.servicemix.bundles/org.apache.servicemix.bundles.jets3t "0.8.1_1"]
-                 ;; End GATK
                  [org.clojure/data.csv "0.1.2"]
                  [me.raynes/fs "1.4.3"]
                  [org.apache.commons/commons-compress "1.5"]
