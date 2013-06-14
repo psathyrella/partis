29747d49bf38a28ebe3779727a8a60815f706baa
Connor McCoy <cmccoy@fhcrc.org>
2013-06-13 17:07:13 -0700

    Factor out mutation

--- src/java/io/github/cmccoy/sam/SAMUtils.java
+++ src/java/io/github/cmccoy/sam/SAMUtils.java
@@ -6,106 +6,55 @@ import java.util.ArrayList;
 import net.sf.samtools.SAMRecord;
 import net.sf.samtools.CigarElement;
 
-import com.google.common.base.Objects;
 import static com.google.common.base.Preconditions.*;
-import com.google.common.collect.ComparisonChain;
+
+import io.github.cmccoy.sam.Mutation.MutationType;
 
 public class SAMUtils {
-  public static class Mutation implements Comparable<Mutation> {
-    private final char type;
-    private final long position;
-    private final char wt;
-    private final char mut;
-    public Mutation(char type, long position, char wt, char mut) {
-      checkArgument(position >= 0, "Negative position: %d", position);
-      this.type = type;
-      this.position = position;
-      this.wt = wt;
-      this.mut = mut;
-    }
 
-    @Override
-    public String toString() {
-      return Objects.toStringHelper(this)
-        .add("type", type)
-        .add("position", position)
-        .add("wt", wt)
-        .add("mut", mut)
-        .toString();
-    }
+    public static List<Mutation> enumerateMutations(final SAMRecord record,
+                                                    final byte[] referenceBases) {
+        checkNotNull(record, "null SAM record");
+        checkNotNull(referenceBases, "null reference bases");
+        // Always be noting 1-based indexing!
+        final int start = record.getAlignmentStart() - 1;
+        final byte[] qBases = record.getReadBases();
+        final List<Mutation> result = new ArrayList<Mutation>();
 
-    @Override
-    public int hashCode() {
-      return Objects.hashCode(type, position, wt, mut);
-    }
+        int qi = 0, ri = start;
 
-    @Override
-    public int compareTo(Mutation other) {
-      return ComparisonChain.start()
-        .compare(this.type, other.type)
-        .compare(this.position, other.position)
-        .compare(this.wt, other.wt)
-        .compare(this.mut, other.mut)
-        .result();
-    }
-    @Override
-    public boolean equals(Object obj) {
-      if (obj == null) return false;
-      if (obj.getClass() != getClass()) return false;
-      final Mutation other = (Mutation) obj;
-
-      return other.type == this.type &&
-        other.position == this.position &&
-        other.wt == this.wt &&
-        other.mut == this.mut;
-    }
-
-    public char getType() { return type; }
-    public long getPosition() { return position; }
-    public char getWildType() { return wt; }
-    public char getMutant() { return mut; }
-  }
-
-  public static List<Mutation> enumerateMutations(final SAMRecord record,
-                                                  final byte[] referenceBases) {
-    checkNotNull(record, "null SAM record");
-    checkNotNull(referenceBases, "null reference bases");
-    // Always be noting 1-based indexing!
-    final int start = record.getAlignmentStart() - 1;
-    final byte[] qBases = record.getReadBases();
-    final List<Mutation> result = new ArrayList<Mutation>();
-
-    int qi = 0, ri = start;
-
-    for(final CigarElement e : record.getCigar().getCigarElements()) {
-      switch (e.getOperator()) {
-        case H:
-        case S:
-          break;
-        case I:
-          result.add(new Mutation('I', ri, '-', (char)qBases[qi]));
-          break;
-        case D:
-          result.add(new Mutation('D', ri, (char)referenceBases[ri], '-'));
-          break;
-        case M:
-          for(int i = 0; i < e.getLength(); ++i) {
-            if(qBases[qi+i] != referenceBases[ri+i]) {
-              result.add(new Mutation('M', ri + i,
-                    (char)referenceBases[ri+i],
-                    (char)qBases[qi+i]));
+        for(final CigarElement e : record.getCigar().getCigarElements()) {
+            switch (e.getOperator()) {
+            case H:
+            case S:
+                break;
+            case I:
+                result.add(new Mutation(MutationType.INSERTION, ri, "-",
+                                        new String(qBases, qi, e.getLength())));
+                break;
+            case D:
+                result.add(new Mutation(MutationType.DELETION, ri,
+					new String(referenceBases, ri, 1),
+					"-"));
+                break;
+            case M:
+                for(int i = 0; i < e.getLength(); ++i) {
+                    if(qBases[qi+i] != referenceBases[ri+i]) {
+                        result.add(new Mutation(MutationType.MUTATION, ri + i,
+						new String(referenceBases, ri + i, 1),
+						new String(qBases, qi + i, 1)));
+                    }
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("unknown " + e.toString());
             }
-          }
-          break;
-        default:
-          throw new IllegalArgumentException("unknown " + e.toString());
-      }
-      final int l = e.getLength();
-      if(e.getOperator().consumesReadBases())
-        qi += l;
-      if(e.getOperator().consumesReferenceBases())
-        ri += l;
+            final int l = e.getLength();
+            if(e.getOperator().consumesReadBases())
+                qi += l;
+            if(e.getOperator().consumesReferenceBases())
+                ri += l;
+        }
+        return result;
     }
-    return result;
-  }
 }
--- test/ighutil/enumerate_mutations_test.clj
+++ test/ighutil/enumerate_mutations_test.clj
@@ -1,20 +1,20 @@
 (ns ighutil.enumerate-mutations-test
-  (:import [io.github.cmccoy.sam SAMUtils$Mutation])
+  (:import [io.github.cmccoy.sam Mutation Mutation$MutationType])
   (:require [clojure.test :refer :all]
             [ighutil.enumerate-mutations :refer :all]))
 
 (defn- mut [ref-idx wt mut]
-  (SAMUtils$Mutation. \M ref-idx wt mut))
+  (Mutation. Mutation$MutationType/MUTATION ref-idx wt mut))
 
 (deftest test-summarize-mutation-partition
   (testing "Simple case"
     (is (= {:v-gene nil
             :j-gene nil
             :cdr3-length nil
-            :edges {"3" ["1" 1],  "2" ["1" 0]}
+            :edges {"3" '(["1" 1]),  "2" '(["1" 0])}
             :n-seqs 3}
            (summarize-mutation-partition
-               [{:name "1" :mutations [(mut 101 \A \C)]}
-                {:name "2" :mutations [(mut 101 \A \C)]}
-                {:name "3" :mutations [(mut 101 \A \C)
-                                       (mut 105 \G \T)]}])))))
+               [{:name "1" :mutations [(mut 101 "A" "C")]}
+                {:name "2" :mutations [(mut 101 "A" "C")]}
+                {:name "3" :mutations [(mut 101 "A" "C")
+                                       (mut 105 "G" "T")]}])))))
