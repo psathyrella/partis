0e49717e129fe710fff47f13766414f355cb26b3
Connor McCoy <cmccoy@fhcrc.org>
2013-06-20 08:14:36 -0700

    IUPAC utilities and test

---
+++ test/ighutil/iupac_utils_test.clj
@@ -0,0 +1,35 @@
+(ns ighutil.iupac-utils-test
+  (:import [io.github.cmccoy.dna IUPACUtils])
+  (:require [clojure.test :refer :all]))
+
+(defn roundtrip [^String s]
+  (let [b (.getBytes s)
+        packed (IUPACUtils/packBytes b)
+        unpacked (IUPACUtils/unpackShorts packed)
+        unpacked-str (String. unpacked)]
+    (is (= s unpacked-str))))
+
+;; All IUPAC
+(def ALL "ACGTMRWSYKVHDBN")
+
+(deftest test-roundtrip
+  (roundtrip "NNNN")
+  (roundtrip ALL))
+
+(defn compatible? [^String s1 ^String s2]
+  (let [p1 (IUPACUtils/packBytes (.getBytes s1))
+        p2 (IUPACUtils/packBytes (.getBytes s2))]
+    (IUPACUtils/areCompatible p1 p2)))
+
+(deftest test-are-compatible
+  (are [x y] (compatible? x y)
+       "NNNN" "NNNN"
+       ALL ALL
+       "NNNN" "AAAA"
+       "AAAA" "NNNN"
+       "R" "D")
+  (are [x y] (not (compatible? x y))
+       "ACGT" "TGCA"
+       "V" "T"
+       "C" "D"
+       "R" "Y"))
