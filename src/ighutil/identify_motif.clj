fcd1ee031431224e0b26e2675f41219ddba76a20
Connor McCoy <cmccoy@fhcrc.org>
2013-06-20 11:27:58 -0700

    Support multiple motifs

--- src/ighutil/identify_motif.clj
+++ src/ighutil/identify_motif.clj
@@ -3,37 +3,38 @@
             ReferenceSequenceFileFactory]
            [io.github.cmccoy.dna IUPACUtils])
   (:require [clojure.java.io :as io]
-            [cliopatra.command :refer [defcommand]]
             [clojure.data.csv :as csv]
+            [clojure.string :as string]
+            [cliopatra.command :refer [defcommand]]
             [ighutil.io :as zio]
             [ighutil.fasta :refer [extract-references]]))
 
-(defn- motif-occurrences [^shorts motif ^bytes ref]
-  "Finds occurrences of motif in ref, allowing for ambiguity"
-  (let [packed-ref (IUPACUtils/packBytes ref)
+(defn- motif-occurrences [^shorts motif ^bytes s]
+  "Finds occurrences of motif in ref, allowing for ambiguity
+  returns a lazy-seq of (location, sequence)"
+  (let [packed (IUPACUtils/packBytes s)
         motif-length (alength motif)
-        ref-length (alength ref)]
-    (when (>= ref-length motif-length)
-      (for [i (range (- ref-length motif-length))
-            :when (IUPACUtils/areCompatible packed-ref motif i motif-length)]
-        [i (String. ref i motif-length)]))))
+        s-length (alength s)]
+    (when (>= s-length motif-length)
+      (for [i (range (- s-length motif-length))
+            :when (IUPACUtils/areCompatible packed motif i motif-length)]
+        [i (String. s (int i) (int motif-length))]))))
 
 (defcommand identify-motif
   "Find occurrences of a motif in a FASTA file"
   {:opts-spec [["-i" "--in-file" "Source FASTA file" :required true
                 :parse-fn io/file]
-               ["-o" "--out-file" "Destination path" :required true
-                :parse-fn io/file]
-               ["-m" "--motif" "Motif (IUPAC Ambiguous)"
+               ["-o" "--out-file" "Destination path" :required true]
+               ["-m" "--motifs" "Motif (IUPAC Ambiguous), comma separated"
                 :required true]]}
-  (let [reference-seqs (-> in-file
-                           ReferenceSequenceFileFactory/getReferenceSequenceFile
-                           extract-references)
-        packed-motif (IUPACUtils/packBytes (.getBytes ^String motif))]
-    (with-open [out (zio/writer out-file)]
-      (csv/write-csv out [["reference" "position" "motif" "sequence"]])
-      (doseq [[ref-name ^bytes ref-bytes] reference-seqs]
-        (println ref-name)
-        (let [occ (->> ref-bytes
-                       (motif-occurrences packed-motif))]
-          (csv/write-csv out (for [[l s] occ] [ref-name l motif s])))))))
+  (with-open [out (zio/writer out-file)]
+    (csv/write-csv out [["reference" "position" "motif" "sequence"]])
+    (let [reference-seqs (-> in-file
+                             ReferenceSequenceFileFactory/getReferenceSequenceFile
+                             extract-references)]
+      (doseq [^String motif (string/split motifs #",")]
+        (let [packed-motif (IUPACUtils/packBytes (.getBytes motif))]
+          (doseq [[ref-name ^bytes ref-bytes] reference-seqs]
+            (let [occ (->> ref-bytes
+                           (motif-occurrences packed-motif))]
+              (csv/write-csv out (for [[l s] occ] [ref-name l motif s])))))))))
