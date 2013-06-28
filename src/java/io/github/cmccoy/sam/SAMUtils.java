0396a66d181ba4ea8d8602eb90300c269f31feee
Connor McCoy <cmccoy@fhcrc.org>
2013-06-28 12:41:35 -0700

    Quick java method

--- src/java/io/github/cmccoy/sam/SAMUtils.java
+++ src/java/io/github/cmccoy/sam/SAMUtils.java
@@ -3,8 +3,12 @@ package io.github.cmccoy.sam;
 import java.util.List;
 import java.util.ArrayList;
 
-import net.sf.samtools.SAMRecord;
+import net.sf.picard.reference.ReferenceSequence;
+import net.sf.picard.reference.ReferenceSequenceFile;
+import net.sf.samtools.AlignmentBlock;
 import net.sf.samtools.CigarElement;
+import net.sf.samtools.SAMRecord;
+import net.sf.samtools.util.PeekIterator;
 
 import static com.google.common.base.Preconditions.*;
 
@@ -67,5 +71,61 @@ public class SAMUtils {
                 ri += l;
         }
         return result;
+    }
+
+    /**
+     * Calculate the probability that each base matches the reference,
+     * given an iterable of equiprobable alignments.
+     *
+     * \param ref Reference sequences
+     * \param records SAM alignments for a given sequence, primary first
+     * \returns 100 * p(base matches)
+     */
+    public static byte[] calculateBaseEqualProbabilities(ReferenceSequenceFile ref,
+                                                         Iterable<SAMRecord> records) {
+      final PeekIterator<SAMRecord> it = new PeekIterator<SAMRecord>(checkNotNull(records).iterator());
+      checkArgument(it.hasNext(), "No records in iterable!");
+      final SAMRecord primary = it.peek();
+      final byte[] bases = primary.getReadBases();
+      checkArgument(!primary.getNotPrimaryAlignmentFlag(), "First record is not primary");
+
+      byte[] matches = new byte[primary.getReadLength()];
+      byte[] tot = new byte[primary.getReadLength()];
+      for(int i = 0; i < primary.getReadLength(); i++) {
+        matches[i] = 0;
+        tot[i] = 0;
+      }
+
+      while(it.hasNext()) {
+        final SAMRecord record = it.next();
+        checkState(record.getReadName().equals(primary.getReadName()),
+            "Unmatched read names in group (%s, %s)",
+            record.getReadName(),
+            primary.getReadName());
+        final String referenceName = record.getReferenceName();
+        final byte[] refBases = ref.getSequence(referenceName).getBases();
+        for(final AlignmentBlock b : record.getAlignmentBlocks()) {
+          // 0-based index in read, ref
+          final int readStart = b.getReadStart() - 1;
+          final int refStart = b.getReferenceStart() - 1;
+          for(int i = 0; i < b.getLength(); i++) {
+            tot[readStart + i]++;
+            if(bases[readStart + i] == refBases[refStart + i])
+              matches[readStart + i]++;
+          }
+        }
+      }
+
+      byte[] result = new byte[matches.length];
+      for(int i = 0; i < tot.length; i++) {
+        if(tot[i] > 0) {
+          float pMatch = ((float)matches[i]) / tot[i];
+          result[i] =  (byte)(100 * pMatch);
+        } else {
+          result[i] = -1;
+        }
+      }
+
+      return result;
     }
 }
