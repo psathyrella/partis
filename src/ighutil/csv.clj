ff5a0d2a7ad0a0cc6c059775f7be363e861dcf20
Connor McCoy <cmccoy@fhcrc.org>
2013-07-08 11:45:43 -0700

    Simplify

--- src/ighutil/csv.clj
+++ src/ighutil/csv.clj
@@ -14,7 +14,8 @@
 (defn csv-to-maps [fp & {:keys [keywordize?]
                          :or {keywordize? true}
                          :as options}]
-  (let [rows (apply (partial csv/read-csv fp)
+  (let [rows (apply csv/read-csv
+                    fp
                     (dissoc options :keywordize?))
         header (->> rows
                     first
@@ -23,7 +24,7 @@
     (map make-row (rest rows))))
 
 (defn read-typed-csv [fp types & csv-opts]
-  (let [rows (apply (partial csv-to-maps fp) csv-opts)
+  (let [rows (apply csv-to-maps fp csv-opts)
         type-entry (fn [[k v]] [k ((get types k identity) v)])
         type-row (fn [m] (->> m (map type-entry) (into {})))]
     (map type-row rows)))
