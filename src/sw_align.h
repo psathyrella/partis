a22c40d992bb3cb3b523935ebe9a8972cbd8717d
Connor McCoy <cmccoy@fhcrc.org>
2013-08-23 11:31:09 -0700

    header

--- src/sw_align.h
+++ src/sw_align.h
@@ -6,10 +6,10 @@
 void align_reads (const char* ref_path,
                   const char* qry_path,
                   const char* output_path,
-                  const int32_t match, /* 2 */
+                  const int32_t match,    /* 2 */
                   const int32_t mismatch, /* 2 */
-                  const int32_t gap_open, /* 3 */
-                  const int32_t gap_extension, /* 1 */
+                  const int32_t gap_o,    /* 3 */
+                  const int32_t gap_e,    /* 1 */
                   const uint8_t n_threads);
 
 #endif
