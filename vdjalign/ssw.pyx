45e7d7b9ba513bea7bf75913747ead19fd746b1b
Connor McCoy <cmccoy@fhcrc.org>
2013-08-22 18:20:04 -0700

    WIP!

--- vdjalign/ssw.pyx
+++ vdjalign/ssw.pyx
@@ -1,117 +1,37 @@
-# distutils: language = c++
+# distutils: language = c
 # distutils: include_dirs = ssw-src/src
-# distutils: sources = ssw-src/src/ssw_cpp.cpp ssw-src/src/ssw.c
+# distutils: sources = ssw-src/src/ssw.c vdjalign/ssw_align.c
 # distutils: extra_compile_args = -fopenmp
 # distutils: extra_link_args = -fopenmp -lz
-import re
-from libcpp.string cimport string
 from libc.stdlib cimport free, malloc
-from libc.string cimport strdup
+from libc.stdint cimport int32_t
 
-cdef extern from "inttypes.h":
-    ctypedef int int32_t
-    ctypedef unsigned short uint16_t
-    ctypedef unsigned char uint8_t
 
-cdef extern from "ssw_cpp.h" namespace "StripedSmithWaterman":
-    cdef cppclass Alignment:
-        int32_t sw_score
-        int32_t sw_score_next_best
-        int32_t ref_begin
-        int32_t ref_end
-        int32_t ref_end_next_best
-        int32_t query_begin
-        int32_t query_end
-        int32_t mismatches
-        string cigar_string
-    cdef cppclass Filter:
-        pass
-    cdef cppclass Aligner:
-        Aligner()
-        Aligner(uint8_t& match_score,
-              uint8_t& mismatch_penalty,
-              uint8_t& gap_opening_penalty,
-              uint8_t& gap_extending_penalty)
+cdef extern from "ssw_align.h":
+    void align_reads(const char*,
+                     const char*,
+                     const char*,
+                     int32_t,
+                     int32_t,
+                     int32_t,
+                     int32_t) nogil
 
-        int Align(char *query, char *rf, int& ref_len,
-                Filter& filter, Alignment* alignment) nogil
+def align_reads_to_ref(bytes ref_path,
+                       bytes qry_path,
+                       bytes output_path,
+                       int match=2,
+                       int mismatch=2,
+                       int gap_open=3,
+                       int gap_extend=1):
+    cdef char* ref = ref_path
+    cdef char* qry = qry_path
+    cdef char* out = output_path
 
-_cigar_re = re.compile(r'(\d+)([A-Z])')
+    cdef int32_t m = match, p = mismatch, go = gap_open, ge = gap_extend
 
-cdef align_to_pair(Alignment a, bytes query, bytes ref):
-    """
-    Convert an alignment (with cigar string) to a tuple containing
-    (aligned_ref, aligned_query)
-    """
-    cdef bytes cigar = a.cigar_string
-    cdef bytes c, clen_str
-    cdef int i, clen, ri, qi, j
-    qi = a.query_begin
-    ri = a.ref_begin
-    q_aln = []
-    r_aln = []
+    with nogil:
+        align_reads(ref, qry, out, m, p, go, ge)
+    free(ref)
+    free(qry)
+    free(out)
 
-    cigar_parts = _cigar_re.findall(cigar)
-    assert cigar_parts
-
-    for clen_str, c in cigar_parts:
-        clen = int(clen_str)
-
-        if c == 'M':
-            for j in range(clen):
-                q_aln.append(query[qi + j])
-                r_aln.append(ref[ri + j])
-            ri += clen
-            qi += clen
-        elif c == 'I':
-            for j in range(clen):
-                q_aln.append(query[qi + j])
-                r_aln.append('-')
-            qi += clen
-        elif c == 'D':
-            for j in range(clen):
-                q_aln.append('-')
-                r_aln.append(ref[ri + j])
-            ri += clen
-
-    return ''.join(r_aln), ''.join(q_aln)
-
-def align(bytes query, bytes ref, parse_cigar=True, uint8_t match_score=2,
-        uint8_t mismatch_penalty=2, uint8_t gap_open_penalty=3, uint8_t gap_extend_penalty=1):
-    """
-    ``align(query, ref, parse_cigar=True)``
-
-    Aligns ``query`` to ``ref`` using banded smith waterman.
-
-    Returns a dict.
-    """
-    cdef Filter f
-    cdef Aligner *aligner = new Aligner(match_score, mismatch_penalty, gap_open_penalty, gap_extend_penalty)
-    cdef Alignment alignment
-    cdef int res, ref_len = len(ref)
-    cdef char *qry_str = strdup(query), *ref_str = strdup(ref)
-
-    try:
-        with nogil:
-            res = aligner.Align(qry_str, ref_str, ref_len, f, &alignment)
-    finally:
-        del aligner
-        free(qry_str)
-        free(ref_str)
-    if not res:
-        raise ValueError("Error in alignment")
-
-    result = {'sw_score': alignment.sw_score,
-              'sw_score_next_best': alignment.sw_score_next_best,
-              'ref_begin': alignment.ref_begin,
-              'ref_end': alignment.ref_end,
-              'ref_end_next_best': alignment.ref_end_next_best,
-              'query_begin': alignment.query_begin,
-              'query_end': alignment.query_end,
-              'mismatches': alignment.mismatches,
-              'cigar_string': alignment.cigar_string}
-    if parse_cigar:
-        r_aln, q_aln = align_to_pair(alignment, query, ref)
-        result['ref_align'] = r_aln
-        result['query_align'] = q_aln
-    return result
---
+++ vdjalign/ssw_align.c
@@ -0,0 +1,229 @@
+/*  main.c
+ *  Created by Mengyao Zhao on 06/23/11.
+ *	Version 0.1.4
+ *  Last revision by Mengyao Zhao on 07/31/12.
+ */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <emmintrin.h>
+#include <zlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+#include "ssw.h"
+#include "kseq.h"
+#include "kvec.h"
+
+#ifdef __GNUC__
+#define LIKELY(x) __builtin_expect((x),1)
+#define UNLIKELY(x) __builtin_expect((x),0)
+#else
+#define LIKELY(x) (x)
+#define UNLIKELY(x) (x)
+#endif
+
+/*! @function
+  @abstract  Round an integer to the next closest power-2 integer.
+  @param  x  integer to be rounded (in place)
+  @discussion x will be modified.
+  */
+#define kroundup32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
+
+KSEQ_INIT(gzFile, gzread);
+
+    /*void reverse_comple(const char* seq, char* rc) {*/
+        /*int32_t end = strlen(seq), start = 0;*/
+        /*int8_t rc_table[128] = {*/
+            /*4, 4,  4, 4,  4,  4,  4, 4,  4, 4, 4, 4,  4, 4, 4, 4,*/
+            /*4, 4,  4, 4,  4,  4,  4, 4,  4, 4, 4, 4,  4, 4, 4, 4,*/
+            /*4, 4,  4, 4,  4,  4,  4, 4,  4, 4, 4, 4,  4, 4, 4, 4,*/
+            /*4, 4,  4, 4,  4,  4,  4, 4,  4, 4, 4, 4,  4, 4, 4, 4,*/
+            /*4, 84, 4, 71, 4,  4,  4, 67, 4, 4, 4, 4,  4, 4, 4, 4,*/
+            /*4, 4,  4, 4,  65, 65, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,*/
+            /*4, 84, 4, 71, 4,  4,  4, 67, 4, 4, 4, 4,  4, 4, 4, 4,*/
+            /*4, 4,  4, 4,  65, 65, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4*/
+        /*};*/
+        /*rc[end] = '\0';*/
+        /*-- end;*/
+        /*while (LIKELY(start < end)) {*/
+            /*rc[start] = (char)rc_table[(int8_t)seq[end]];*/
+            /*rc[end] = (char)rc_table[(int8_t)seq[start]];*/
+            /*++ start;*/
+            /*-- end;*/
+        /*}*/
+        /*if (start == end) rc[start] = (char)rc_table[(int8_t)seq[start]];*/
+    /*}*/
+
+void ssw_write_sam (FILE* dest,
+                    const s_align* a,
+                    const kseq_t* ref_seq,
+                    const kseq_t* read,
+                    const char* read_seq,
+                    const int8_t* table) {
+
+        fprintf(dest, "%s\t", read->name.s);
+        if (a->score1 == 0) fprintf(dest, "4\t*\t0\t255\t*\t*\t0\t0\t*\t*\n");
+        else {
+            int32_t c, l = a->read_end1 - a->read_begin1 + 1, qb = a->ref_begin1, pb = a->read_begin1, p;
+            uint32_t mapq = -4.343 * log(1 - (double)abs(a->score1 - a->score2)/(double)a->score1);
+            mapq = (uint32_t) (mapq + 4.99);
+            mapq = mapq < 254 ? mapq : 254;
+            fprintf(dest, "0\t");
+            fprintf(dest, "%s\t%d\t%d\t", ref_seq->name.s, a->ref_begin1 + 1, mapq);
+            for (c = 0; c < a->cigarLen; ++c) {
+                int32_t letter = 0xf&*(a->cigar + c);
+                int32_t length = (0xfffffff0&*(a->cigar + c))>>4;
+                fprintf(dest, "%d", length);
+                if (letter == 0) fprintf(dest, "M");
+                else if (letter == 1) fprintf(dest, "I");
+                else fprintf(dest, "D");
+            }
+            fprintf(dest, "\t*\t0\t0\t");
+            for (c = a->read_begin1; c <= a->read_end1; ++c) fprintf(dest, "%c", read_seq[c]);
+            fprintf(dest, "\t");
+            if (read->qual.s) {
+                p = a->read_begin1;
+                for (c = 0; c < l; ++c) {
+                    fprintf(dest, "%c", read->qual.s[p]);
+                    ++p;
+                }
+            } else fprintf(dest, "*");
+            fprintf(dest, "\tAS:i:%d", a->score1);
+            mapq = 0;	// counter of difference
+            for (c = 0; c < a->cigarLen; ++c) {
+                int32_t letter = 0xf&*(a->cigar + c);
+                int32_t length = (0xfffffff0&*(a->cigar + c))>>4;
+                if (letter == 0) {
+                    for (p = 0; p < length; ++p){
+                        if (table[(int)*(ref_seq->seq.s + qb)] != table[(int)*(read_seq + pb)]) ++mapq;
+                        ++qb;
+                        ++pb;
+                    }
+                } else if (letter == 1) {
+                    pb += length;
+                    mapq += length;
+                } else {
+                    qb += length;
+                    mapq += length;
+                }
+            }
+            fprintf(dest,"\tNM:i:%d\t", mapq);
+            if (a->score2 > 0) fprintf(dest, "ZS:i:%d\n", a->score2);
+            else fprintf(dest, "\n");
+        }
+}
+
+void align_reads (const char* ref_path, const char* qry_path,
+                  const char* output_path,
+                  const int32_t match, /* 2 */
+                  const int32_t mismatch, /* 2 */
+                  const int32_t gap_open, /* 3 */
+                  const int32_t gap_extension) { /* 1 */
+    clock_t start, end;
+    float cpu_time;
+    gzFile read_fp, ref_fp, out_fp;
+    kseq_t *read_seq, *ref_seq;
+    int32_t l, m, k, n = 5, s1 = 1048576, s2 = 128, filter = 0;
+    int8_t* mata = (int8_t*)calloc(25, sizeof(int8_t)), *mat = mata;
+    int8_t* ref_num = (int8_t*)malloc(s1);
+    int8_t* num = (int8_t*)malloc(s2);
+
+    /* This table is used to transform nucleotide letters into numbers. */
+    int8_t table[128] = {
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  3, 0, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  3, 0, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
+    };
+
+    // initialize scoring matrix for genome sequences
+    for (l = k = 0; LIKELY(l < 4); ++l) {
+        for (m = 0; LIKELY(m < 4); ++m) mata[k++] = l == m ? match : -mismatch;	/* weight_match : -weight_mismatch */
+        mata[k++] = 0; // ambiguous base
+    }
+    for (m = 0; LIKELY(m < 5); ++m) mata[k++] = 0;
+
+    read_fp = gzopen(qry_path, "r");
+
+    // Read reference sequences
+    kvec_t(kseq_t*) ref_seqs;
+    kv_init(ref_seqs);
+
+    ref_fp = gzopen(ref_path, "r");
+    ref_seq = kseq_init(ref_fp);
+    while ((l = kseq_read(ref_seq)) >= 0) {
+        fprintf(stderr, "Read %s\n", ref_seq->name.s);
+        kv_push(kseq_t*, ref_seqs, ref_seq);
+        ref_seq = kseq_init(ref_fp);
+    }
+    kseq_destroy(ref_seq); // One extra allocated
+    gzclose(ref_fp);
+
+    // Print SAM header
+    out_fp = gzopen(output_path, "w");
+    fprintf(out_fp, "@HD\tVN:1.4\tSO:queryname\n");
+    for(l = 0; l < kv_size(ref_seqs); l++) {
+        ref_seq = kv_A(ref_seqs, l);
+        fprintf(out_fp, "@SQ\tSN:%s\tLN:%d\n", ref_seq->name.s, (int32_t)ref_seq->seq.l);
+    }
+
+    read_seq = kseq_init(read_fp);
+
+    // alignment
+    start = clock();
+    while (kseq_read(read_seq) >= 0) {
+        s_profile* p;
+        int32_t readLen = read_seq->seq.l;
+        int32_t maskLen = readLen / 2;
+        //	int32_t maskLen = 2*readLen;
+
+        while (readLen >= s2) {
+            ++s2;
+            kroundup32(s2);
+            num = (int8_t*)realloc(num, s2);
+        }
+        for (m = 0; m < readLen; ++m) num[m] = table[(int)read_seq->seq.s[m]];
+        p = ssw_init(num, readLen, mat, n, 2);
+
+        for(l = 0; l < kv_size(ref_seqs); ++l) {
+            ref_seq = kv_A(ref_seqs, l);
+            s_align* result;
+            int32_t refLen = ref_seq->seq.l;
+            int8_t flag = 0;
+            while (refLen > s1) {
+                ++s1;
+                kroundup32(s1);
+                ref_num = (int8_t*)realloc(ref_num, s1);
+            }
+            for (m = 0; m < refLen; ++m) ref_num[m] = table[(int)ref_seq->seq.s[m]];
+            flag = 2;
+            result = ssw_align (p, ref_num, refLen, gap_open, gap_extension, flag, filter, 0, maskLen);
+            ssw_write_sam(out_fp, result, ref_seq, read_seq, read_seq->seq.s, table);
+            align_destroy(result);
+        }
+
+        init_destroy(p);
+    }
+
+
+    end = clock();
+    cpu_time = ((float) (end - start)) / CLOCKS_PER_SEC;
+    fprintf(stderr, "CPU time: %f seconds\n", cpu_time);
+
+    kseq_destroy(read_seq);
+    // Clean up reference sequences
+    for(l = 0; l < kv_size(ref_seqs); ++l)
+        free(kv_A(ref_seqs, l));
+    kv_destroy(ref_seqs);
+
+    gzclose(read_fp);
+    free(num);
+    free(ref_num);
+    free(mata);
+}
