d6dcfa055922ac80a3cb82560750679588563d75
Connor McCoy <cmccoy@fhcrc.org>
2013-10-25 15:16:36 -0700

    Add a PG line

--- python/src/ig_align.c
+++ python/src/ig_align.c
@@ -471,6 +471,8 @@ void ig_align_reads(const char *ref_path,
     // Print SAM header
     out_fp = fopen(output_path, "w");
     fprintf(out_fp, "@HD\tVN:1.4\tSO:unsorted\n");
+    fprintf(out_fp, "@PG\tID:ig_align\tPN:ig_align\tCL:match=%d,mismatch=%d,go=%d,ge=%d\n",
+            match, mismatch, gap_o, gap_e);
     for(size_t i = 0; i < kv_size(ref_seqs); i++) {
         seq = &kv_A(ref_seqs, i);
         fprintf(out_fp, "@SQ\tSN:%s\tLN:%d\n",
