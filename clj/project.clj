0e88d90982d61ddce411cabbb09425fa7b8293c7
Connor McCoy <cmccoy@fhcrc.org>
2014-03-24 08:51:17 -0700

    Bump deps

--- clj/project.clj
+++ clj/project.clj
@@ -3,8 +3,8 @@
   :license {:name "GPL v3"
             :url "https://www.gnu.org/licenses/gpl.txt"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.chapmanb/picard "1.96"]
-                 [org.clojars.chapmanb/sam "1.96"]
+                 [org.clojars.chapmanb/picard "1.107"]
+                 [org.clojars.chapmanb/sam "1.107"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/math.combinatorics "0.0.7"]
                  [me.raynes/fs "1.4.5"]
