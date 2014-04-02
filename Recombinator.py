08df85723a5eda1c7e2b70a9a68f64ddba2c412e
Duncan Ralph <dkralph@gmail.com>
2014-04-02 10:23:38 -0700

    Refactor and write info to CSV

--- Recombinator.py
+++ Recombinator.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 """ Simulates the process of VDJ recombination """ 
 import sys
+import csv
+import json
 import random
 import numpy
 from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.Alphabet import generic_alphabet
 
+#----------------------------------------------------------------------------------------
 def int_to_nucleotide(number):
     """ Convert between (0,1,2,3) and (A,C,G,T) """
     if number == 0:
@@ -21,6 +24,7 @@ def int_to_nucleotide(number):
         print 'ERROR nucleotide number not in [0,3]'
         sys.exit()
 
+#----------------------------------------------------------------------------------------
 class Recombinator(object):
     """ Simulates the process of VDJ recombination """
 
@@ -31,31 +35,47 @@ class Recombinator(object):
     mean_insertion_length = 6  # mean length of the non-templated insertion
 
     all_seqs = {}  # all the Vs, all the Ds...
-    seqs = {}   # one of each
     regions = ['v', 'd', 'j']
-    version_freq_table = {}
+    version_freq_table = {}  # list of the probabilities with which each VDJ combo appears in data
 
     def __init__(self):
-        """ Initialize from files """ 
+        """ Initialize from files """
         for region in self.regions:
-            self.read_vdj_versions(region, 'igh'+region+'.fasta')
+            self.read_vdj_versions(region, 'data/igh'+region+'.fasta')
         self.read_vdj_version_freqs('version-counter/filtered-vdj-probs.txt')
+        with open('data/v-meta.json','r') as jfile:
+            self.json_data = json.load(jfile)
 
-    def combine(self):
+    def combine(self, outfile=''):
         """ Run the combination. Returns the new sequence. """
-        vdj_combo_label = self.choose_vdj_combo()  # a tuple with the names of the chosen versions
+        vdj_combo_label = self.choose_vdj_combo()  # a tuple with the names of the chosen versions (v_gene, d_gene, j_gene, cdr3_length)
+        reco_info = {}  # collect information about the recombination process for output to csv file
+        reco_info['vdj_combo'] = vdj_combo_label
+        chosen_seqs = {}
         for region in self.regions:
-            self.seqs[region] = self.get_seq(region, vdj_combo_label)
+            chosen_seqs[region] = self.get_seq(region, vdj_combo_label)
+        cysteine_position = self.json_data[vdj_combo_label[0]]['cysteine-position']
+        print '  cysteine position: %d' % cysteine_position
         print '  eroding'
-        self.erode('right', self.seqs['v'], 'v')
-        self.erode('left', self.seqs['d'], 'd')
-        self.erode('right', self.seqs['d'], 'd')
-        self.erode('left', self.seqs['j'], 'j')
-        vd_seq = self.join(self.seqs['v'], self.get_insertion(), self.seqs['d'])
-        recombined_seq = self.join(vd_seq, self.get_insertion(), self.seqs['j'])
-        final_seq = self.mutate(recombined_seq)
+        chosen_seqs['v'] = self.erode(reco_info, 'right', chosen_seqs['v'], 'v', cysteine_position)
+        chosen_seqs['d'] = self.erode(reco_info, 'left', chosen_seqs['d'], 'd')
+        chosen_seqs['d'] = self.erode(reco_info, 'right', chosen_seqs['d'], 'd')
+        chosen_seqs['j'] = self.erode(reco_info, 'left', chosen_seqs['j'], 'j')
+        reco_info['vd_insertion'] = self.get_insertion()
+        vd_seq = self.join(chosen_seqs['v'], reco_info['vd_insertion'], chosen_seqs['d'])
+        reco_info['dj_insertion'] = self.get_insertion()
+        recombined_seq = self.join(vd_seq, reco_info['dj_insertion'], chosen_seqs['j'])
+        reco_info['mutations'] = ''
+        final_seq = self.mutate(reco_info, recombined_seq, cysteine_position)
+        reco_info['seq'] = final_seq
         print '  before mute:',recombined_seq
         print '  after mute: ',final_seq
+        cysteine_word = str(final_seq[cysteine_position:cysteine_position+3])
+        if cysteine_word != 'TGT' and cysteine_word != 'TGC':
+            print 'ERROR cysteine in V is messed up, you probably eroded it off: %s' % cysteine_word
+            sys.exit()
+        if outfile != '':
+            self.write_csv(outfile, reco_info)
         return final_seq
 
     def read_vdj_versions(self, region, fname):
@@ -64,7 +84,10 @@ class Recombinator(object):
         """
         self.all_seqs[region] = {}
         for seq_record in SeqIO.parse(fname, "fasta"):
-            self.all_seqs[region][seq_record.name] = seq_record.seq
+            # This line works equally well without the string conversion
+            # ... er, but it seems simpler to not use the fancy class if'n
+            # I don't need it? It's the same speed both ways.
+            self.all_seqs[region][seq_record.name] = str(seq_record.seq)
 
     def read_vdj_version_freqs(self, fname):
         """ Read the frequencies at which various VDJ combinations appeared
@@ -89,14 +112,12 @@ class Recombinator(object):
         sum_prob = 0.0
         print '  choosing combination',
         for vdj_choice in self.version_freq_table:
-#            print '  += %.2e for %60s' % (self.version_freq_table[vdj_choice], vdj_choice),
             sum_prob += self.version_freq_table[vdj_choice]
-#            print ' %.3e < %.3e ? %d' % (iprob, sum_prob, iprob < sum_prob)
             if iprob < sum_prob:
                 print ' ---> ',vdj_choice
                 return vdj_choice
         print 'ERROR I shouldn\'t be here'
-        sys.exit()  # shouldn't get here!
+        sys.exit()
 
     def get_seq(self, region, vdj_combo_label):
         gene_name = ''
@@ -119,27 +140,39 @@ class Recombinator(object):
         # NOTE casting to an int reduces the mean somewhat
         return int(numpy.random.exponential(self.mean_nukes_eroded))
 
-    def erode(self, location, seq, region):
+    def erode(self, reco_info, location, seq, region, cysteine_position=-1):
         """ Erode (delete) some number of letters from the <location> side of
         <seq>, where location is 'left' or 'right'
         """
         n_to_erode = self.get_n_to_erode()
+        if cysteine_position > 0:
+            if location != 'right' or region != 'v':
+                print 'ERROR that doesn\'t make sense'
+                sys.exit()
+            while len(seq) - n_to_erode - 3 <= cysteine_position:
+                print '    about to erode off cysteine (%d), try again' % n_to_erode
+                n_to_erode = self.get_n_to_erode()
+
+        reco_info[region + '_' + location] = n_to_erode
         fragment_before = ''
         fragment_after = ''
         if location == 'left':
             fragment_before = seq[:n_to_erode + 3] + '...'
-            seq = seq[n_to_erode:len(seq)]
-            fragment_after = seq[:n_to_erode + 3] + '...'
+            new_seq = seq[n_to_erode:len(seq)]
+            fragment_after = new_seq[:n_to_erode + 3] + '...'
         elif location == 'right':
             fragment_before = '...' + seq[len(seq) - n_to_erode - 3 :]
-            seq = seq[0:len(seq)-n_to_erode]
-            fragment_after = '...' + seq[len(seq) - n_to_erode - 3 :]
+            new_seq = seq[0:len(seq)-n_to_erode]
+            fragment_after = '...' + new_seq[len(new_seq) - n_to_erode - 3 :]
         else:
             print 'ERROR location must be \"left\" or \"right\"'
             sys.exit()
         print '    %3d from %5s' % (n_to_erode, location),
         print ' of %s region: %15s' % (region, fragment_before),
         print ' --> %-15s' % fragment_after
+        if len(fragment_after) == 0:
+            print '    NOTE eroded away entire sequence'
+        return new_seq
 
     def get_insertion(self):
         """ Get the non-templated sequence to insert between
@@ -150,7 +183,7 @@ class Recombinator(object):
         for _ in range(0, length):
             insert_seq_str += int_to_nucleotide(random.randint(0, 3))
         print '  inserting %d: %s' % (length, insert_seq_str)
-        return Seq(insert_seq_str, generic_alphabet)
+        return insert_seq_str
 
     def join(self, left_seq, insert_sequence, right_seq):
         """ Join three sequences in the indicated order.
@@ -161,18 +194,45 @@ class Recombinator(object):
         print '  gives: %s' % new_seq
         return new_seq
 
-    def mutate(self, seq):
+    def mutate(self, reco_info, seq, cysteine_position):
         """ Apply point mutations to <seq>, then return it """
         n_mutes = int(numpy.random.poisson(self.mute_rate*len(seq)))
         original_seq = seq
-        print '  adding %3d point mutations' % n_mutes
+        print '  adding %d point mutations' % n_mutes
         for _ in range(n_mutes):
             position = random.randint(0, len(seq)-1)
+            while (position == cysteine_position or
+                   position == (cysteine_position + 1) or
+                   position == (cysteine_position + 2)):
+                print '  position is in cysteine codon (%d), trying again.' % position
+                position = random.randint(0, len(seq)-1)
             old_nuke = seq[position]
             new_nuke = old_nuke
             while new_nuke == old_nuke:
                 new_nuke = int_to_nucleotide(random.randint(0,3))
             print '    muting %s to %s at position %3d' % (old_nuke, new_nuke, position)
+            reco_info['mutations'] += '%d%s%s-' % (position, old_nuke, new_nuke)
             seq = seq[:position] + new_nuke + seq[position+1:]
 
+        reco_info['mutations'] = reco_info['mutations'].rstrip('-')
         return seq
+
+    def write_csv(self, outfile, reco_info):
+        with open(outfile, 'ab') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            # NOTE this is the *original* cdr3_length, i.e. from Connor's file, *not* the length in the sequence I'm spitting out
+            # TODO fix that shit
+            columns = ('vdj_combo', 'v_right', 'd_left', 'd_right', 'j_left', 'vd_insertion', 'dj_insertion', 'mutations', 'seq')
+            csv_row = []
+            for column in columns:
+                print column,
+                if column == 'vdj_combo':
+                    csv_row.append(reco_info[column][0])
+                    csv_row.append(reco_info[column][1])
+                    csv_row.append(reco_info[column][2])
+                else:
+                    csv_row.append(reco_info[column])
+                    
+            csv_writer.writerow(csv_row)
+#            print csv_row
+                    
--- run-reco.py
+++ run-reco.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+
 from Recombinator import Recombinator
+
 #----------------------------------------------------------------------------------------
-#
 reco = Recombinator()
-for _ in range(20):
+for _ in range(30):
     print 'combining...'
-    raw_reco_seq = reco.combine()
+    raw_reco_seq = reco.combine('out.csv')
     print 'final %s' % raw_reco_seq
