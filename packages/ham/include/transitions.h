ac979ea14be81782114b578c99ffd5e61bd8d9fb
Duncan Ralph <dkralph@gmail.com>
2014-11-06 11:42:34 -0800

    deobfuscify ham topology checker

--- packages/ham/include/transitions.h
+++ packages/ham/include/transitions.h
@@ -17,7 +17,7 @@ public:
   Transition(string to_state, double prob);
 
   inline void set_name(string& txt) { to_state_name_ = txt; }
-  inline void set_state(State* st) { to_state_ = st; }
+  inline void set_to_state(State* st) { to_state_ = st; }
   inline string &to_state_name() {return to_state_name_;};
   inline State *to_state() {return to_state_;};
 
@@ -25,9 +25,9 @@ public:
 
   void Print();
 private:
-  string to_state_name_;  //What state we are transitioning to (Fill out when parsing)
-  State* to_state_;    // pointer to the to-state (Filled during HMM finalization)
-  double log_prob_; //Log of standard transition probability
+  string to_state_name_;  // the state to which we are transitioning (set during parsement)
+  State *to_state_;  // pointer to the to-state (set in model::FinalizeState)
+  double log_prob_;  // log transition probability
 };
 
 }
