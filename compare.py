cc9d38b6c6cbb44ff8a1d15e4020447c45dc1456
Duncan Ralph <dkralph@gmail.com>
2014-11-14 17:08:14 -0800

    git file cleanups

---
+++ compare.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('python')
+
+import plotting
+
+plotdir = '/var/www/sharing/dralph/partis/few'
+plotting.compare_directories(plotdir + '/hmm-vs-sw',
+                             plotdir + '/hmm/plots', 'hmm',
+                             plotdir + '/sw/plots', 'sw',
+                             xtitle='inferred - true')
