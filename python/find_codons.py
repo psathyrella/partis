e7921e4392c5c1d7e00742ff59c822259e1cf81e
Duncan Ralph <dkralph@gmail.com>
2014-11-17 17:05:17 -0800

    clean up codon finder

--- python/find_codons.py
+++ python/find_codons.py
@@ -6,7 +6,6 @@ import utils
 from opener import opener
 
 dirname = 'data/imgt'
-# seqfname = dirname + '/ighv.fasta'
 align_fname = dirname + '/ighv-aligned.fasta'
 
 def get_info(fname):
@@ -15,7 +14,6 @@ def get_info(fname):
         info[seq_record.name] = str(seq_record.seq)
     return info
 
-# seqinfo = get_info(seqfname)
 align_info = get_info(align_fname)
 
 with opener('r')(dirname + '/v-meta.json') as json_file:  # get location of <begin> cysteine in each v region
@@ -25,12 +23,7 @@ align_cpos_aa = 104 - 1  # cysteine position in amino acids (-1 to convert to ze
 align_cpos = align_cpos_aa * 3
 
 for name, align_seq in align_info.items():  # NOTE there's a whole bunch of sequences in seqinfo that aren't in aliinfo. If imgt doesn't have alignment info for 'em, though, I'm ignoring 'em
-    # if name not in seqinfo:
-    #     print '  WARNING %s not in seqinfo' % name
-    #     continue
-    # seq = seqinfo[name]
     print '%-20s' % name,
-
     # check for unexpected characters
     if 'N' in align_seq:
         print '\n    WARNING replacing N with A'
@@ -61,3 +54,11 @@ for name, align_seq in align_info.items():  # NOTE there's a whole bunch of sequ
             print 'not the same, new: %d old: %s' % (real_cpos, cyst_positions[name]['cysteine-position'])
     else:
         print 'new: %d' % (real_cpos)
+        cyst_positions[name] = {}
+        cyst_positions[name]['cysteine-position'] = real_cpos
+
+outfname = 'out.json'
+outfile = open(outfname, 'w')
+json.dump(cyst_positions, outfile, indent=0)
+outfile.close()
+print 'wrote output to', outfname
