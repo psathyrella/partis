379d8f722db82bce699cb1d5027373ed4992e6f4
Duncan Ralph <dkralph@gmail.com>
2014-12-28 14:43:45 -0800

    fully disable joint/pair emission
    having two conflicting ways to decare emissions in the yamels was
    just... bad. I don't like that this makes it so you can't specify joint
    emission, but you could only specify joint emission for k=2 before,
    anyway, and it was confusing. One of the best things about how the k-hmm
    is implemented is that it runs cleanly on however many sequences you
    pass it, deciding what k was on the fly from the number of sequences,
    and the pair-hmm joint table in the yamel totally subverts that
    model. So it dies. For now.
    
    NOTE the yamels in bcrham/test/data/regression still have pair
    emissions, they're just ignored. I suppose I should add a test that
    there aren't tags in the input yamels that I don't use, but it doesn't
    seem important at the moment.

--- packages/ham/include/emission.h
+++ packages/ham/include/emission.h
@@ -17,16 +17,14 @@ namespace ham {
 class Emission {
 public:
   Emission();
-  void Parse(YAML::Node config, string is_pair, Tracks model_tracks);
+  void Parse(YAML::Node config, Tracks model_tracks);
   ~Emission();
 
   inline double score(Sequence *seq, size_t pos) { return scores_.LogProb(seq, pos); }
-  inline double score(Sequences *seqs, size_t pos) { return scores_.LogProb(seqs, pos); }
 
   void Print();
 private:
   double total_;
-  bool pair_;
   LexicalTable scores_;
   vector<Track*>* tracks_; // tracks on to which this emission is wont to spit
 };
