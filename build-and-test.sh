6cd3d3c9aa61d29c9545e8a6bf2b3a81655229bc
Erick Matsen <matsen@fhcrc.org>
2015-01-03 13:07:07 -0800

    Making more directories in build-and-test

--- build-and-test.sh
+++ build-and-test.sh
@@ -2,12 +2,12 @@ set -eu
 
 export LEIN_ROOT=1
 cd /partis/packages/samtools/ && make
-mkdir /partis/_bin
+mkdir -p /partis/_bin
 ln -s $PWD/samtools /partis/_bin
 cd /partis/packages/ighutil/ && make -C clj
 pip install --user ./python
 cd /partis/packages/ham/ && scons bcrham
 cd /partis/
-mkdir -p /true/plots
+mkdir -p /plots /v-per-base/plots/ /d-per-base/plots/ /j-per-base/plots/ /partis/_output/test/plots/params/data
 export PATH=/partis/_bin:$PATH
 scons test
