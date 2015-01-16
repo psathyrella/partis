d119bfdcbdd639601ef43f7a5a57e64062b930bd
Duncan Ralph <dkralph@gmail.com>
2015-01-16 11:05:35 -0800

    unbreak ham build

--- packages/ham/src/hample.cc
+++ packages/ham/src/hample.cc
@@ -30,7 +30,6 @@ int main(int argc, const char *argv[]) {
     cerr << "ERROR: " << e.error() << " for argument " << e.argId() << endl;
     throw;
   }
-  return 1;
 
   // read hmm model file
   Model hmm;
