06002ac7b4281a1d5922890cfa42a1ef7efff5c9
Duncan Ralph <dkralph@gmail.com>
2015-01-23 12:08:32 -0800

    add exception for invalid alphabet character

--- include/track.h
+++ include/track.h
@@ -16,22 +16,19 @@ class Tracks;
 class Track {
   friend class Tracks;
 public:
-  Track() : track_index_(numeric_limits<size_t>::max()) {}
+  Track(){}
   Track(string, vector<string>);
   void set_name(string nm) { name_ = nm; }
-  void set_index(size_t index) { assert(index < numeric_limits<size_t>::max());  track_index_ = index; }
   void AddSymbol(string symbol);
   void AddSymbols(vector<string> &symbols);
 
   string name() { return name_; }
-  size_t index() { return track_index_; }
   size_t alphabet_size() { return alphabet_.size(); }
-  string symbol(size_t iter) { return alphabet_.at(iter); }  // return <iter>th element of <alphabet_> (which is probably a letter, but could be several letters or something else)
+  string symbol(size_t iter) { return alphabet_.at(iter); }  // return <iter>th element of <alphabet_> (which is probably a letter, but could be several letters or something else). NOTE throws std:out_of_range exception if <iter> is invalide
   uint8_t symbol_index(const string &symbol);
   string Stringify();
 private:
   string name_;
-  size_t track_index_;
   vector<string> alphabet_;  // vector of this track's allowed symbols (eg {A,C,G,T})
   map<string, uint8_t> symbol_indices_;
 };
--- src/track.cc
+++ src/track.cc
@@ -4,8 +4,8 @@ namespace ham {
 
 // ----------------------------------------------------------------------------------------
   Track::Track(string name, vector<string> symbols) :
-  name_(name),
-  track_index_(numeric_limits<size_t>::max()) {
+  name_(name)
+  {
   AddSymbols(symbols);
 }
 
@@ -24,9 +24,9 @@ void Track::AddSymbols(vector<string> &symbols) {
 
 // ----------------------------------------------------------------------------------------
 uint8_t Track::symbol_index(const string &symbol) {
-  // if(symbol_indices_.count(symbol) == 0) {
-  //   throw runtime_error("ERROR symbol '" + symbol + "' not found among " + Stringify());
-  // }
+  if(symbol_indices_.count(symbol) == 0) {
+    throw runtime_error("ERROR symbol '" + symbol + "' not found among " + Stringify());
+  }
   return symbol_indices_[symbol];
 }
 
