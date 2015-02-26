0cd348b03e9334e579d796b6005f1d5f4a83bc77
Duncan Ralph <dkralph@gmail.com>
2015-02-25 17:53:20 -0800

    backup-my-work commmit

--- packages/ham/src/jobholder.cc
+++ packages/ham/src/jobholder.cc
@@ -202,7 +202,7 @@ Result JobHolder::Run(Sequences seqs, KBounds kbounds) {
   // print debug info
   if(debug_) {
     if(algorithm_ == "viterbi") {
-      cout << "    " << setw(48) << seqs.name_str();
+      cout << "       " << setw(48) << seqs.name_str();
       cout << "   " << kbounds.vmin << "-" << kbounds.vmax - 1 << "   " << kbounds.dmin << "-" << kbounds.dmax - 1; // exclusive...
       cout << "    best kset: " << setw(4) << best_kset.v << setw(4) << best_kset.d << setw(12) << best_score << endl;
     } else {
