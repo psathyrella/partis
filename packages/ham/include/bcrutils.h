9c70c5c4bd621c3fa08c8570378fabf118eb48f8
Duncan Ralph <dkralph@gmail.com>
2015-04-07 12:54:56 -0700

    rename jobholder to dphandler, plus more ham reorgainzation

--- packages/ham/include/bcrutils.h
+++ packages/ham/include/bcrutils.h
@@ -3,11 +3,15 @@
 
 #include <string>
 #include <map>
+#include <set>
 #include <cassert>
+#include <sstream>
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <cmath>
+
+#include "model.h"
 
 using namespace std;
 namespace ham {
@@ -57,7 +61,7 @@ public:
 };
 
 // ----------------------------------------------------------------------------------------
-class RecoEvent {  // class to keep track of recombination event. Initially, just to allow printing. Translation of print_reco_event in utils.py
+class RecoEvent {  // keeps track of recombination event. Initially, just to allow printing. Translation of print_reco_event in utils.py
 public:
   RecoEvent();
   map<string, string> genes_;
@@ -82,5 +86,68 @@ public:
   void Clear() { genes_.clear(); deletions_.clear(); insertions_.clear(); }
   void Print(GermLines &germlines, size_t cyst_position = 0, size_t final_tryp_position = 0, bool one_line = false, string extra_indent = "");
 };
+
+// ----------------------------------------------------------------------------------------
+class KSet {  // pair of k_v,k_d values specifying how to chop up the query sequence into v+insert, d+insert, j []
+public:
+  KSet(size_t k_v, size_t k_d) : v(k_v), d(k_d) {}
+  bool equals(KSet rhs) { return v == rhs.v && d == rhs.d; }
+  bool operator< (const KSet rhs) const { return  v < rhs.v && d < rhs.d; }
+
+  size_t v;
+  size_t d;
+};
+
+// ----------------------------------------------------------------------------------------
+class KBounds {
+public:
+  KBounds() {}  // need this so we can use stl vectors
+  KBounds(KSet kmin, KSet kmax) : vmin(kmin.v), dmin(kmin.d), vmax(kmax.v), dmax(kmax.d) {}
+  bool equals(KBounds rhs) { return vmin == rhs.vmin && vmax == rhs.vmax && dmin == rhs.dmin && dmax == rhs.dmax; }
+  KBounds LogicalOr(KBounds rhs);  // return the "logical OR" of <kb1> and <kb2>, i.e. the area encompassed by either of 'em
+  string stringify() {
+    stringstream ss;
+    ss << vmin << "-" << vmax << ", " << dmin << "-" << dmax;
+    return ss.str();
+  }
+  size_t vmin, dmin, vmax, dmax;
+};
+// ----------------------------------------------------------------------------------------
+class HMMHolder {
+public:
+  HMMHolder(string hmm_dir, GermLines &gl): hmm_dir_(hmm_dir), gl_(gl) {}
+  ~HMMHolder();
+  Model *Get(string gene, bool debug);
+  // Rescale, within each hmm, the emission probabilities to reflect <overall_mute_freq> instead of the mute freq which was recorded in the hmm file.
+  // If <overall_mute_freq> is -INFINITY, we re-rescale them to what they were originally
+  void RescaleOverallMuteFreqs(map<string, set<string> > &only_genes, double overall_mute_freq);  // WOE BETIDE THEE WHO FORGETETH TO RE-RESET THESE
+  void UnRescaleOverallMuteFreqs(map<string, set<string> > &only_genes);
+  void CacheAll();  // read all available hmms into memory
+private:
+  string hmm_dir_;
+  GermLines &gl_;
+  map<string, Model*> hmms_; // map of gene name to hmm pointer
+};
+
+// ----------------------------------------------------------------------------------------
+class Result {
+public:
+  Result(KBounds kbounds) : total_score_(-INFINITY), no_path_(false), better_kbounds_(kbounds), boundary_error_(false), could_not_expand_(false) {}
+  void check_boundaries(KSet best, KBounds kbounds);  // and if you find errors, put expanded bounds in better_[kmin,kmax]_
+  bool boundary_error() { return boundary_error_; } // is the best kset on boundary of k space?
+  bool could_not_expand() { return could_not_expand_; }
+  KBounds better_kbounds() { return better_kbounds_; }
+  double total_score() { return total_score_; }
+  double total_score_;
+  bool no_path_;
+  vector<RecoEvent> events_;
+
+private:
+  KBounds better_kbounds_;
+  bool boundary_error_;
+  bool could_not_expand_;
+};
+
 }
+
 #endif
---
+++ packages/ham/include/dphandler.h
@@ -0,0 +1,57 @@
+#ifndef DPHANDLER_H
+#define DPHANDLER_H
+
+#include <map>
+#include <string>
+#include <sstream>
+#include <math.h>
+#include <set>
+#include <iomanip>
+#include <stdexcept>
+
+#include "trellis.h"
+#include "mathutils.h"
+#include "bcrutils.h"
+#include "args.h"
+
+using namespace std;
+namespace ham {
+// ----------------------------------------------------------------------------------------
+class DPHandler {
+public:
+  DPHandler(Args *args, GermLines &gl, HMMHolder &hmms, vector<string> only_genes = {});
+  ~DPHandler();
+  void Clear();
+  Result Run(string algorithm, Sequences seqs, KBounds kbounds, double overall_mute_freq = -INFINITY);  // run all over the kspace specified by bounds in kmin and kmax
+  Result Run(string algorithm, Sequence seq, KBounds kbounds, double overall_mute_freq = -INFINITY);
+  void StreamOutput(double test);  // print csv event info to stderr
+  // void WriteBestGeneProbs(ofstream &ofs, string query_name);
+
+private:
+  void RunKSet(string algorithm, Sequences &seqs, KSet kset, map<KSet, double> *best_scores, map<KSet, double> *total_scores, map<KSet, map<string, string> > *best_genes);
+  void FillTrellis(string algorithm, Sequences query_seqs, vector<string> query_strs, string gene, double *score, string &origin);
+  void PushBackRecoEvent(Sequences &seqs, KSet kset, map<string, string> &best_genes, double score, vector<RecoEvent> *events);
+  RecoEvent FillRecoEvent(Sequences &seqs, KSet kset, map<string, string> &best_genes, double score);
+  vector<string> GetQueryStrs(Sequences &seqs, KSet kset, string region);
+
+  void PrintPath(vector<string> query_strs, string gene, double score, string extra_str = "");
+  Sequences GetSubSeqs(Sequences &seqs, KSet kset, string region);
+  map<string, Sequences> GetSubSeqs(Sequences &seqs, KSet kset);  // get the subsequences for the v, d, and j regions given a k_v and k_d
+  void SetInsertions(string region, string query_str, vector<string> path_names, RecoEvent *event);
+  size_t GetInsertStart(string side, size_t path_length, size_t insert_length);
+  size_t GetInsertLength(string side, vector<string> names);
+  size_t GetErosionLength(string side, vector<string> names, string gene_name);
+
+  Args *args_;
+  string hmm_dir_;  // location of .hmm files
+  GermLines &gl_;
+  HMMHolder &hmms_;
+  map<string, set<string> > only_genes_;
+
+  map<string, map<vector<string>, trellis*> > trellisi_; // collection of the trellises we've calculated, so we can reuse them. eg: trellisi_["IGHV1-18*01"]["ACGGGTCG"] for single hmms, or trellisi_["IGHV1-18*01"][("ACGGGTCG","ATGGTTAG")] for pair hmms
+  map<string, map<vector<string>, TracebackPath*> > paths_; // collection of the paths.
+  map<string, map<vector<string>, double> > all_scores_;
+  map<string, double> best_per_gene_scores_;
+};
+}
+#endif
