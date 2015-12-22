683af9d4bdffde6a40d3925515e36cf0d696a46e
Duncan Ralph <dkralph@gmail.com>
2015-12-21 16:18:50 -0800

    finish auto germline initialization script

--- bin/initialize-germline-set.py
+++ bin/initialize-germline-set.py
@@ -14,7 +14,7 @@ import utils
 # ----------------------------------------------------------------------------------------
 def clean_dir():
     if os.path.exists(args.dirname):
-        for fname in files_to_copy + [unaligned_fname, aligned_fname]:
+        for fname in files_to_copy + [unaligned_fname, aligned_fname, cyst_fname, error_fname]:
             if os.path.exists(args.dirname + '/' + fname):
                 os.remove(args.dirname + '/' + fname)
         remaining_files = glob.glob(args.dirname + '/*')
@@ -54,38 +54,64 @@ def align_new_genes(old_aligned_genes, genes_without_alignments, all_new_genes):
     os.remove(all_fname)
 
 # ----------------------------------------------------------------------------------------
+def get_n_gaps_up_to_cpos(aligned_seq, cpos):
+    """ return number of gapped positions in <aligned_seq> before <cpos> """
+    ipos = 0  # position in unaligned sequence
+    n_gaps_passed = 0  # number of gapped positions in the aligned sequence that we pass before getting to cpos (i.e. while ipos < cpos)
+    while ipos < cpos:
+        if aligned_seq[ipos + n_gaps_passed] in utils.gap_chars:
+            n_gaps_passed += 1
+        else:
+            ipos += 1
+
+    return n_gaps_passed
+
+# ----------------------------------------------------------------------------------------
+def get_cpos_in_alignment(aligned_seq, seq, cpos):
+    """ given <cpos> in <seq>, find the cysteine's position in <aligned_seq> """
+    utils.check_conserved_cysteine(seq, cpos)
+    cpos_in_alignment = cpos + get_n_gaps_up_to_cpos(aligned_seq, cpos)
+    utils.check_conserved_cysteine(aligned_seq, cpos_in_alignment)
+    return cpos_in_alignment
+
+# ----------------------------------------------------------------------------------------
 def write_cyst_file(known_cyst_positions):
     unaligned_genes = utils.read_germlines(args.dirname, only_region='v')['v']
     aligned_genes = utils.read_germlines(args.dirname, only_region='v', aligned=True)['v']
 
-    common_gene = None  # we need to find at least one gene that's in the old and the new sets, so we know how to convert cyst positions
+    known_gene = None  # we need to find at least one gene that's in the old and the new sets, so we know how to convert cyst positions
     for gene, info in known_cyst_positions.items():
         if gene in aligned_genes:
-            common_gene = gene
+            known_gene = gene
             break
-    if common_gene is None:
+    if known_gene is None:
         raise Exception('couldn\'t find any genes in common between %s and %s, so can\'t write new cyst position file' % (args.reference, args.dirname + '/' + aligned_fname))
 
-    aligned_seq = aligned_genes[common_gene]
-    seq = unaligned_genes[common_gene]
-    cpos = known_cyst_positions[common_gene]['cysteine-position']
-    utils.check_conserved_cysteine(seq, cpos)
-    cpos_in_alignment = cpos
-    ipos = 0  # position in unaligned sequence
-    n_dots_passed = 0  # number of gapped positions in the aligned sequences that we pass before getting to cpos (i.e. while ipos < cpos)
-    while ipos < cpos:
-        if aligned_seq[ipos + n_dots_passed] in utils.gap_chars:
-            cpos_in_alignment += 1
-            n_dots_passed += 1
-        else:
-            ipos += 1
-    utils.check_conserved_cysteine(aligned_seq, cpos_in_alignment)
-    displacement = cpos_in_alignment - cpos
-    print '  cpos displacement: %d' % displacement
-    cyst_positions = []
+    known_cpos = known_cyst_positions[known_gene]['cysteine-position']
+    cpos_in_alignment = get_cpos_in_alignment(aligned_genes[known_gene], unaligned_genes[known_gene], known_cpos)
+    cyst_positions = {}
+    errors = []
     for gene, seq in unaligned_genes.items():
-        
-        cyst_positions.append({'gene' : gene, 'cyst_start' : cpos})
+        unaligned_cpos = cpos_in_alignment - utils.count_gaps(aligned_genes[gene], istop=cpos_in_alignment)
+        try:
+            utils.check_conserved_cysteine(seq, unaligned_cpos, debug=True)
+        except:
+            print '  %s cysteine not found in %s, skipping' % (utils.color('red', 'warning'), gene)
+            # print gene, unaligned_cpos
+            # print seq
+            # print aligned_genes[gene]
+            errors.append(gene)
+            continue
+        cyst_positions[gene] = unaligned_cpos
+
+    with open(args.dirname + '/' + cyst_fname, 'w') as cystfile:
+        writer = csv.DictWriter(cystfile, ('gene', 'cyst_start'))
+        writer.writeheader()
+        for gene, cpos in cyst_positions.items():
+            writer.writerow({'gene' : gene, 'cyst_start' : cpos})
+    with open(args.dirname + '/' + error_fname, 'w') as errorfile:
+        for gene in errors:
+            errorfile.write('%s\n' % gene)
 
 # ----------------------------------------------------------------------------------------
 parser = argparse.ArgumentParser()
@@ -97,10 +123,16 @@ if args.dirname is None:
     args.dirname = os.path.os.path.splitext(args.ighv_fname)[0]
 
 files_to_copy = ['ighd.fasta', 'ighj.fasta', 'j_tryp.csv']
+print 'TODO rename j_tryp to j-tryp'
 unaligned_fname = 'ighv.fasta'
 aligned_fname = 'ighv-aligned.fasta'
+cyst_fname = 'v-cyst.csv'
+error_fname = 'bad-genes.txt'
 
 # ----------------------------------------------------------------------------------------
+clean_dir()
+shutil.copyfile(args.ighv_fname, args.dirname + '/' + unaligned_fname)
+
 # figure out which v genes we need to align
 old_aligned_genes = utils.read_germlines(args.reference_dir, only_region='v', aligned=True)
 all_new_genes = utils.read_germlines(args.dirname, only_region='v')  # all genes in ighv_fname, not just the new ones
@@ -109,12 +141,10 @@ for gene in all_new_genes['v']:
     if gene not in old_aligned_genes['v']:
         genes_without_alignments[gene] = all_new_genes['v'][gene]
 
-# clean_dir()
-# shutil.copyfile(args.ighv_fname, args.dirname + '/' + unaligned_fname)
-# if len(genes_without_alignments) > 0:
-#     align_new_genes(old_aligned_genes['v'], genes_without_alignments, all_new_genes['v'])
-# for fname in files_to_copy:
-#     shutil.copyfile(args.reference_dir + '/' + fname, args.dirname + '/' + fname)
+if len(genes_without_alignments) > 0:
+    align_new_genes(old_aligned_genes['v'], genes_without_alignments, all_new_genes['v'])
+for fname in files_to_copy:
+    shutil.copyfile(args.reference_dir + '/' + fname, args.dirname + '/' + fname)
 
 known_cyst_positions = utils.read_cyst_positions(args.reference_dir)
 write_cyst_file(known_cyst_positions)
