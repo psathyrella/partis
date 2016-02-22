bdddd5a57b4fe161d6a9943d33be08d37b15541a
Duncan Ralph <dkralph@gmail.com>
2016-02-22 10:13:49 -0800

    minor deverbosification

--- python/partitiondriver.py
+++ python/partitiondriver.py
@@ -1285,9 +1285,8 @@ class PartitionDriver(object):
 
                 # get a new dict in which we have edited the sequences to swap Ns on either end (after removing fv and jf insertions) for v_5p and j_3p deletions
                 eroded_line = utils.reset_effective_erosions_and_effective_insertions(self.glfo, padded_line)
-                if eroded_line['invalid']:
+                if eroded_line['invalid']:  # not really sure why the eroded line is sometimes invalid when the padded line is not, but it's very rare and I don't really care, either
                     n_invalid_events += 1
-                    print '      %s eroded line invalid (but padded line not!)' % eroded_line['unique_ids']
                     continue
 
                 padded_annotations[':'.join(padded_line['unique_ids'])] = padded_line
