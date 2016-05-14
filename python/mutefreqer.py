5785773670e70cc32b4abb43cacd090f5cefd12a
Duncan Ralph <dkralph@gmail.com>
2016-05-13 18:54:37 -0700

    remove unused allele-finding fcns

--- python/mutefreqer.py
+++ python/mutefreqer.py
@@ -35,12 +35,10 @@ class MuteFreqer(object):
         # tigger stuff
         self.small_number = 1e-5
         self.dbg_pos = None #20
-        self.tigger = True
+        self.tigger = False
         self.n_max_mutes = 20
-        self.n_std_devs = 2
         self.n_max_bins_to_exclude = self.n_max_mutes - 8  # try excluding up to this many bins (on the left) when doing the fit (leaves at least 8 points for fit)
         self.n_obs_min = 10
-        self.min_y_intercept = 1./8
 
         self.default_slope_bounds = (-0.2, 0.2)
         self.big_y_icpt_bounds = (0.3, 1.5)
@@ -109,72 +107,7 @@ class MuteFreqer(object):
         return residual_sum
 
     # ----------------------------------------------------------------------------------------
-    def get_chi_squared(self, xvals, yvals, slope, intercept):
-        def expected(x):
-            return slope * x + intercept
-        chi_squared = sum([(y - expected(x))**2 / expected(x) for x, y in zip(xvals, yvals)])
-        return chi_squared
-
-    # # ----------------------------------------------------------------------------------------
-    # def get_r_squared(self, xvals, yvals, slope, intercept):
-    #     ybar = float(sum(yvals)) / len(yvals)
-    #     numer = sum([(slope * x + intercept - ybar)**2 for x in xvals])
-    #     denom = sum([(y - ybar)**2 for y in yvals])
-    #     # print '    %8.3f / %8.3f = %8.3f' % (numer, denom, numer / denom)
-    #     return numer / denom
-
-    # ----------------------------------------------------------------------------------------
-    def get_fit(self, n_mutelist, freqs, weights, errs):
-        if len(n_mutelist) < 3:
-            return {'print_str' : '    %9s +/- %-9s   %7s +/- %7s    %7s' % ('', '', '', '', '')}
-        
-        # if freqs.count(0.) == len(freqs):  # no need to fit if it's all zeros
-        #     slope = 0.
-
-        params, cov = numpy.polyfit(n_mutelist, freqs, 1, w=weights, cov=True)
-        slope, slope_err = params[0], math.sqrt(cov[0][0])
-        y_icpt, y_icpt_err = params[1], math.sqrt(cov[1][1])
-
-        # r_squared = self.get_r_squared(n_mutelist, freqs, slope, y_icpt)
-        # chi_squared = self.get_chi_squared(n_mutelist, freqs, slope, y_icpt)
-        residual_sum = self.get_residual_sum(n_mutelist, freqs, errs, slope, y_icpt)
-        ndof = len(n_mutelist) - 1
-    
-        x_icpt, x_icpt_err = 0, 0
-        # interesting = y_icpt - 3*y_icpt_err > self.min_y_intercept  # if y_icpt minus 3 std devs don't get you below the min, it's probably a snp
-        # if interesting:
-        #     x_icpt, x_icpt_err = 0, 0
-        # else:
-        #     x_icpt = -y_icpt / slope
-        #     x_icpt_err = abs(y_icpt / slope) * math.sqrt((y_icpt_err/y_icpt)**2 + (slope_err/slope)**2)
-        #     # err_squared = x_icpt_err * x_icpt_err
-        #     # mean_x_icpt['sum'] += x_icpt / err_squared
-        #     # mean_x_icpt['total'] += 1. / err_squared
-
-        fitfo = {
-            'slope'  : slope,
-            'x_icpt' : x_icpt,
-            'y_icpt' : y_icpt,
-            'slope_err'  : slope_err,
-            'x_icpt_err' : x_icpt_err,
-            'y_icpt_err' : y_icpt_err,
-            # 'r_squared' : r_squared,
-            # 'chi_squared' : chi_squared,
-            'residuals_over_ndof' : float(residual_sum) / ndof,
-            'print_str' : '    %9.3f +/- %-9.3f   %7.4f +/- %7.4f    %7.4f' % (y_icpt, y_icpt_err, slope, slope_err, float(residual_sum) / ndof),
-            'n_mutelist' : n_mutelist,
-            'freqs' : freqs,
-            'errs' : errs
-        }
-
-        return fitfo
-
-    # ----------------------------------------------------------------------------------------
     def get_curvefit(self, pos, n_mutelist, freqs, weights, errs, y_icpt_bounds=None):
-        # if pos == 20:
-        #     for l in [n_mutelist, freqs, weights, errs]:
-        #         print '    %25s   %s' % ('', ' '.join(['%8.3f' % r for r in l]))
-
         def func(x, slope, y_icpt):
             return slope*x + y_icpt
 
@@ -216,101 +149,6 @@ class MuteFreqer(object):
         n_mutelist = [nm for nm in gpcounts['tigger'].keys() if nm < self.n_max_mutes]
 
         return {'obs' : obs, 'total' : total, 'n_mutelist' : n_mutelist, 'freqs' : freqs, 'errs' : errs, 'weights' : weights}
-
-    # ----------------------------------------------------------------------------------------
-    def big_y_intercept(self, y_icpt, y_icpt_err):
-        return y_icpt - self.n_std_devs*y_icpt_err > self.min_y_intercept  # if y_icpt minus two std devs don't get you below the min, it's probably a snp
-
-    # ----------------------------------------------------------------------------------------
-    def find_istart_of_interest(self, residuals, y_icpts, y_icpt_errs, debug=False):
-        min_residual_ratio = 2.
-
-        # ----------------------------------------------------------------------------------------
-        def interesting_residuals(istart):
-            # require that the fits gets dramatically better at this point
-            if residuals[istart-1] / residuals[istart] < min_residual_ratio:
-                if debug:
-                    print 'residual ratio too small %.2f / %.2f = %.2f < %.2f' % (residuals[istart-1], residuals[istart], residuals[istart-1] / residuals[istart], min_residual_ratio)
-                return False
-
-            left_residuals = residuals[:istart]
-            right_residuals = residuals[istart + 1 :]
-
-            left_residual_mean = float(sum(left_residuals)) / len(left_residuals)
-            right_residual_mean = float(sum(right_residuals)) / len(right_residuals)
-            # require that fits are much worse to left of here than to right of here
-            if left_residual_mean / right_residual_mean < min_residual_ratio:
-                if debug:
-                    print 'fits to left aren\'t enough worse than to the right: %.2f / %.2f = %.2f < %.2f' % (left_residual_mean, right_residual_mean, left_residual_mean / right_residual_mean, min_residual_ratio)
-                return False
-
-            return True
-
-        # ----------------------------------------------------------------------------------------
-        def interesting_y_icpts(istart):
-            # require this y intercept to be big
-            if not self.big_y_intercept(y_icpts[istart], y_icpt_errs[istart]):
-                if debug:
-                    print 'y intercept not big enough %.2f - %d*%.2f = %.2f' % (y_icpts[istart], self.n_std_devs, y_icpt_errs[istart], y_icpts[istart] - self.n_std_devs*y_icpt_errs[istart])
-                return False
-
-            # require that the mean y intercept of fits above here is also big
-            if y_icpt_errs.count(0.) > 0:
-                print istart
-                print y_icpts
-                print y_icpt_errs
-            weighted_sum = sum([y_icpt / (err*err) for y_icpt, err in zip(y_icpts[istart + 1 :], y_icpt_errs[istart + 1 :])])
-            sum_squared_weights = sum([1. / (err*err) for err in y_icpt_errs[istart + 1 :]])
-            mean_y_icpt = weighted_sum / sum_squared_weights
-            mean_y_icpt_err = 1. / math.sqrt(sum_squared_weights)
-            if not self.big_y_intercept(mean_y_icpt, mean_y_icpt_err):
-                if debug:
-                    print 'mean y intercept too small to the right of here %.2f - %d*%.2f = %.2f' % (mean_y_icpt, self.n_std_devs, mean_y_icpt_err, mean_y_icpt - self.n_std_devs*mean_y_icpt_err)
-                return False
-
-            return True
-            
-        # ----------------------------------------------------------------------------------------
-        istart_of_interest = None
-        if debug:
-            print '   %3s  %7s  %7s  %7s   ' % ('istart', 'res.', 'y-icpt', 'err')
-        for istart in range(len(residuals)):
-            if debug:
-                print '      %3d  %7.3f  %7.3f  %7.3f   ' % (istart, residuals[istart], y_icpts[istart], y_icpt_errs[istart]),
-            if istart == 0 or istart == len(residuals) - 1:  # zeroth position is never interesting, and we need at least one value to the right in order to determine NOTE fcns above assume left-lists and right-lists are both always of non-zero length
-                if debug:
-                    print ''
-                continue
-            if interesting_y_icpts(istart) and interesting_residuals(istart):
-                # if istart_of_interest is not None:
-                #     raise Exception('  multiple istarts of interest')
-                if debug:
-                    print utils.color('red', 'interesting!')
-                istart_of_interest = istart
-
-        return istart_of_interest
-
-    # ----------------------------------------------------------------------------------------
-    def find_positions_of_interest(self, fitfo, positions_to_fit, debug=False):
-        def get_listfit(pos, key):
-            return [fitfo[istart][pos][key] if pos in fitfo[istart] else None for istart in fitfo]
-
-        interesting_positions = {}
-        for position in positions_to_fit:
-            # debug = position == self.dbg_pos
-            fitlists = {k : get_listfit(position, k) for k in ['residuals_over_ndof', 'y_icpt', 'y_icpt_err']}
-            if debug:
-                print '%3d' % position
-                # for key in fitlists:
-                #     print '    %25s   %s' % (key, ' '.join(['%8.3f' % r for r in fitlists[key]]))
-            istart_of_interest = self.find_istart_of_interest(fitlists['residuals_over_ndof'], fitlists['y_icpt'], fitlists['y_icpt_err'], debug=debug)
-            if istart_of_interest is not None:
-                print '  interesting: istart %d for position %d' % (istart_of_interest, position)
-                if position not in interesting_positions:
-                    interesting_positions[position] = []
-                interesting_positions[position].append(istart_of_interest)
-
-        return interesting_positions
 
     # ----------------------------------------------------------------------------------------
     def finalize_tigger(self, debug=False):
@@ -363,6 +201,7 @@ class MuteFreqer(object):
                 mean_ratios[istart] = score
 
             sorted_istart_rankings = sorted(mean_ratios.items(), key=operator.itemgetter(1), reverse=True)
+            print 'TODO require all of the candidate snp ratios are >2 or something'
             print '    snps    ratio'
             for istart, ratio in sorted_istart_rankings:
                 print '    %2d     %7.2f' % (istart, ratio)
