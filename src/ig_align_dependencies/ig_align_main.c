37b6268736988a820cca01eb4eb120927ca4da26
Annak17 <annak17@lakesideschool.org>
2016-06-20 14:18:41 -0700

    Testing ig_align.

---
+++ src/ig_align_dependencies/ig_align_main.c
@@ -0,0 +1,8 @@
+//#include "ig_align.c"
+#include <stdio.h>
+#include <zlib.h>
+int main(int argc, const char *argv[]) {
+  /*  ig_align_reads("igkv.fasta", 0, NULL, "iglv.fasta", "output.txt", 2, 2, 3,
+     1,
+                   1000, 0, 150, 1, NULL, NULL); */
+}
