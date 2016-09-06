81e641db5dc41d6c6be04babb373593688846056
Duncan Ralph <dkralph@gmail.com>
2016-09-05 17:31:11 -0700

    refactor allele finding tester

--- bin/test-allele-finding.py
+++ bin/test-allele-finding.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+import argparse
+import time
 import sys
 import os
 import glob
-import random
+
 from subprocess import check_call
 sys.path.insert(1, './python')
 
@@ -28,10 +30,8 @@ def get_label(existing_genes, new_allele):
     return '_existing_' + join_gene_names(existing_genes) + '_new_' + join_gene_names(new_allele)
 
 # ----------------------------------------------------------------------------------------
-def run_test(simulation_v_genes, inference_v_genes, dj_genes, seed=None):
-    if seed is not None:
-        random.seed(seed)
-
+def run_test(args):
+    print 'seed %d' % args.seed
     label = 'test'  #get_label(existing_genes, new_allele)
     simfname = outdir + '/simu-' + label + '.csv'
     outpdir = outdir + '/simu-' + label
@@ -41,14 +41,19 @@ def run_test(simulation_v_genes, inference_v_genes, dj_genes, seed=None):
         plotdir = '_www/partis/allele-finding/' + label
 
     # simulate
-    if True:
-        simevents = 8000
-        cmd_str = base_cmd + ' simulate --n-sim-events ' + str(simevents) + ' --n-leaves 1 --constant-number-of-leaves --simulate-partially-from-scratch --mutation-multiplier 0.5 --outfname ' + simfname
-        # cmd_str += ' --n-procs 10'
-        cmd_str += ' --subsimproc --slurm --n-procs 30'
+    if not args.nosim:
+        cmd_str = base_cmd + ' simulate --n-sim-events ' + str(args.n_sim_events) + ' --n-leaves 1 --constant-number-of-leaves --simulate-partially-from-scratch --mutation-multiplier 0.5 --outfname ' + simfname
+        if args.slurm:
+            cmd_str += ' --n-procs 30 --slurm'
+        else:
+            cmd_str += ' --n-procs 10'
 
-        if simulation_v_genes is not None:
-            simulation_genes = simulation_v_genes + ':' + dj_genes
+        if args.gen_gset:
+            cmd_str += ' --generate-germline-set'
+            cmd_str += ' --n-genes-per-region 1:5:3'
+            cmd_str += ' --n-alleles-per-gene 2,3:1,2:1,2'
+        else:
+            simulation_genes = args.sim_v_genes + ':' + args.dj_genes
             sglfo = glutils.read_glfo('data/germlines/human', chain=chain, only_genes=simulation_genes.split(':'), debug=True)
             # snps_to_add = [
             #     {'gene' : 'IGHV4-59*01', 'positions' : (94, 30, 138, 13, 62, 205, 77, 237, 93, 218, 76, 65, 31, 120, 22, 216, 79, 56, 109)},
@@ -56,47 +61,50 @@ def run_test(simulation_v_genes, inference_v_genes, dj_genes, seed=None):
             # glutils.add_some_snps(snps_to_add, sglfo, debug=True)
             glutils.write_glfo(outdir + '/germlines/simulation', sglfo)
             cmd_str += ' --initial-germline-dir ' + outdir + '/germlines/simulation'
-        else:
-            cmd_str += ' --generate-germline-set'
-            cmd_str += ' --n-genes-per-region 1:5:3'
-            cmd_str += ' --n-alleles-per-gene 2,3:1,2:1,2'
 
-        if seed is not None:
-            cmd_str += ' --seed ' + str(seed)
+        if args.seed is not None:
+            cmd_str += ' --seed ' + str(args.seed)
         run(cmd_str)
 
     # remove any old sw cache files
-    sw_cachefiles = glob.glob(outpdir + '/sw-cache-*.csv')
+    sw_cachefiles = glob.glob(outpdir + '/sw-cache-*')
     if len(sw_cachefiles) > 0:
-        check_call(['rm', '-v'] + sw_cachefiles)
+        check_call(['rm', '-rv'] + sw_cachefiles)
 
     # generate germline set and cache parameters
     cmd_str = base_cmd + ' cache-parameters --infname ' + simfname + ' --only-smith-waterman --debug-allele-finding'
-    # cmd_str += ' --n-procs 10'
-    cmd_str += ' --n-procs 30 --slurm'
+    if args.slurm:
+        cmd_str += ' --n-procs 30 --slurm'
+    else:
+        cmd_str += ' --n-procs 10'
 
-    if inference_v_genes is not None:
-        inference_genes = inference_v_genes + ':' + dj_genes
+    if args.gen_gset:
+        cmd_str += ' --generate-germline-set'
+    else:
+        inference_genes = args.inf_v_genes + ':' + args.dj_genes
         iglfo = glutils.read_glfo('data/germlines/human', chain=chain, only_genes=inference_genes.split(':'), debug=True)
         glutils.write_glfo(outdir + '/germlines/inference', iglfo)
         cmd_str += ' --initial-germline-dir ' + outdir + '/germlines/inference'
         cmd_str += ' --find-new-alleles'  # --new-allele-fname ' + outdir + '/new-alleles.fa'
-    else:
-        cmd_str += ' --generate-germline-set'
 
     cmd_str += ' --parameter-dir ' + outpdir
     cmd_str += ' --plotdir ' + plotdir
-    if seed is not None:
-        cmd_str += ' --seed ' + str(seed)
+    if args.seed is not None:
+        cmd_str += ' --seed ' + str(args.seed)
     run(cmd_str)
 
 # ----------------------------------------------------------------------------------------
+parser = argparse.ArgumentParser()
+parser.add_argument('--nosim', action='store_true')
+parser.add_argument('--n-sim-events', type=int, default=300)
+parser.add_argument('--seed', type=int, default=int(time.time()))
+parser.add_argument('--gen-gset', action='store_true')
+parser.add_argument('--dj-genes', default='IGHD6-19*01:IGHJ4*02')
+parser.add_argument('--sim-v-genes', default='IGHV4-39*01')
+parser.add_argument('--inf-v-genes', default='IGHV4-39*06')
+parser.add_argument('--slurm', action='store_true')
+args = parser.parse_args()
+if not args.gen_gset is None:
+    args.sim_v_genes += ':' + args.inf_v_genes
 
-seed = None  # 1
-dj_genes = 'IGHD6-19*01:IGHJ4*02'
-inference_v_genes = None #'IGHV4-30-2*05' #'IGHV1-18*01'
-simulation_v_genes = None #inference_v_genes + ':IGHV4-30-2*03' # + ':IGHV9-99*02'  # 'IGHV4-59*01:IGHV4-59*04' # + IGHV1-18*01'
-
-run_test(simulation_v_genes, inference_v_genes, dj_genes, seed=seed)
-
-# allelic_groups = utils.separate_into_allelic_groups(glfo['seqs'])
+run_test(args)
