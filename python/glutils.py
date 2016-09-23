6830ac098813c2ae0e6b0ef725be75f6525774df
Duncan Ralph <dkralph@gmail.com>
2016-09-23 11:22:35 -0700

    handle already-removed genes

--- python/glutils.py
+++ python/glutils.py
@@ -488,12 +488,16 @@ def remove_genes(glfo, genes, debug=False):
 # ----------------------------------------------------------------------------------------
 def remove_gene(glfo, gene, debug=False):
     """ remove <gene> from <glfo> """
-    if debug:
-        print '  removing %s from glfo' % utils.color_gene(gene)
     region = utils.get_region(gene)
     if region in utils.conserved_codons[glfo['chain']]:
         del glfo[utils.conserved_codons[glfo['chain']][region] + '-positions'][gene]
-    del glfo['seqs'][region][gene]
+    if gene in glfo['seqs'][region]:
+        if debug:
+            print '  removing %s from glfo' % utils.color_gene(gene)
+        del glfo['seqs'][region][gene]
+    else:
+        if debug:
+            print '  can\'t remove %s from glfo, it\'s not there' % utils.color_gene(gene)
 
 # ----------------------------------------------------------------------------------------
 def add_new_alleles(glfo, newfos, remove_template_genes=False, debug=False):
