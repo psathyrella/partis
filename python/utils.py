cfe54d6e04907462d17f314191960629b754f5b2
Duncan Ralph <dkralph@gmail.com>
2017-01-23 18:55:40 -0800

    gingerly start refactoring utils.print_reco_event()

--- python/utils.py
+++ python/utils.py
@@ -16,6 +16,7 @@ import copy
 from opener import opener
 import seqfileopener
 import glutils
+import prutils
 
 #----------------------------------------------------------------------------------------
 # NOTE I also have an eps defined in hmmwriter. Simplicity is the hobgoblin of... no, wait, that's just plain ol' stupid to have two <eps>s defined
@@ -1049,77 +1050,7 @@ def print_seq_in_reco_event(germlines, original_line, iseq, extra_str='', label=
             print '        %s can\'t yet print multiple indels' % color('yellow', 'warning')
         add_indels_to_germline_strings(lengths, glseqs, line, indelfo)
 
-    # ----------------------------------------------------------------------------------------
-    def process_position(original, final):
-        if original not in expected_characters or final not in expected_characters:
-            raise Exception('one of %s %s not among expected characters' % (original, final))
-
-        if original in ambiguous_bases or final in ambiguous_bases:
-            return final
-
-        if original != final:
-            return color('red', final)
-
-        return final
-
-    # build up the query sequence line, including colors for mutations and conserved codons
-    j_right_extra = ''  # portion of query sequence to right of end of the j match
-    n_inserted = 0
-    final_seq_list = []
-    if indelfo is not None:
-        lastfo = indelfo['indels'][-1]  # if the "last" (arbitrary but necessary ordering) indel starts here
-    for inuke in range(len(lseq)):
-        # if we're at the position that the insertion started at (before we removed it)
-        if indelfo is not None and lastfo['type'] == 'insertion':
-            if inuke == lastfo['pos']:
-                final_seq_list.append(lastfo['seqstr'])  # put the insertion back into the query sequence
-                n_inserted += len(lastfo['seqstr'])
-        if indelfo is not None and lastfo['type'] == 'deletion':
-            if inuke - lastfo['pos'] >= 0 and inuke - lastfo['pos'] < lastfo['len']:  # if we're within the bases that we added to make up for the deletionlen
-                final_seq_list.append(color('light_blue', '*'))
-                continue
-
-        new_nuke = ''
-        key = None
-        ilocal = inuke
-        if indelfo is not None:
-            ilocal += n_inserted
-        if ilocal < len(line['fv_insertion']):  # haven't got to start of v match yet, so just add on the query seq nuke
-            pass
-        else:
-            ilocal -= len(line['fv_insertion'])
-            if ilocal < lengths['v']:
-                key = 'v_gl_seq'
-            else:
-                ilocal -= lengths['v']
-                if ilocal < len(line['vd_insertion']):
-                    key = 'vd_insertion'
-                else:
-                    ilocal -= len(line['vd_insertion'])
-                    if ilocal < lengths['d']:
-                        key = 'd_gl_seq'
-                    else:
-                        ilocal -= lengths['d']
-                        if ilocal < len(line['dj_insertion']):
-                            key = 'dj_insertion'
-                        else:
-                            ilocal -= len(line['dj_insertion'])
-                            if ilocal < lengths['j']:
-                                key = 'j_gl_seq'
-                            else:
-                                j_right_extra += ' '
-
-        if key is None:
-            original = lseq[inuke]  # dummy value
-        else:
-            original = glseqs[key][ilocal] if key in glseqs else line[key][ilocal]
-        new_nuke = process_position(original, lseq[inuke])
-
-        for region, pos in line['codon_positions'].items():  # reverse video for the conserved codon positions
-            if inuke >= pos and inuke < pos + 3:
-                new_nuke = '\033[7m' + new_nuke + '\033[m'
-
-        final_seq_list.append(new_nuke)
+    final_seq_list, j_right_extra = prutils.get_query_line(lseq, line, lengths, glseqs, indelfo=indelfo)
 
     # check if there isn't enough space for dots in the vj line
     no_space = False
