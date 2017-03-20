fb65e50892ff81c734dd5f82602fbaec6c5aadca
Duncan Ralph <dkralph@gmail.com>
2017-03-20 12:28:07 -0700

    fiddle with cf-germline-inference

--- test/cf-germline-inference.py
+++ test/cf-germline-inference.py
@@ -15,30 +15,50 @@ baseoutdir = fsdir + '/dralph/partis/allele-finder'
 locus = 'igh'
 region = 'v'
 
-# ----------------------------------------------------------------------------------------
-sys.path.insert(1, './datascripts')
-import heads
-for study in ['kate-qrs-2016-09-09', 'laura-mb-2016-12-22', 'chaim-donor-45-2016-08-04', 'adaptive-billion-read-2016-04-07', 'vollmers-2016-04-08']:
-# for study in ['adaptive-billion-read-2016-04-07']:
-    names, dirs = [], []
-    metafo = heads.read_metadata(study)
-    print study
-    for dset in metafo:
-        bdir = fsdir + '/processed-data/partis/' + study + '/vz/' + dset
-        if metafo[dset]['timepoint'] == 'merged':
-            continue
-        if dset == 'Hs-LN3-5RACE-IgG':  # bad one
-            continue
-        if not os.path.exists(bdir):
-            print '    %s missing' % dset
-            continue
-        names.append(metafo[dset]['shorthand'])
-        dirs.append(bdir + '/plots/sw/mute-freqs/overall')
-    check_call(['./bin/compare-plotdirs.py', '--outdir', fsdir + '/dralph/partis/tmp/lots-of-mfreqs/' + study, '--plotdirs', ':'.join(dirs), '--names', ':'.join(names)])
-# check_call(['./bin/compare-plotdirs.py', '--outdir', fsdir + '/dralph/partis/tmp/lots-of-mfreqs/zeroths', '--plotdirs', ':'.join(dirs), '--names', ':'.join(names)])
-sys.exit()
+# # ----------------------------------------------------------------------------------------
+# sys.path.insert(1, './datascripts')
+# import heads
+# label = 'vz'
+# ptype = 'sw'
+# subject = None  # 'GMC'
+# studies = [
+#     'kate-qrs-2016-09-09',
+#     'laura-mb-2016-12-22',
+#     'chaim-donor-45-2016-08-04',
+#     'adaptive-billion-read-2016-04-07',
+#     'vollmers-2016-04-08',
+#     # 'jason-mg-2017-02-01',
+#     # 'jason-influenza-2017-02-03',
+# ]
 
-# ----------------------------------------------------------------------------------------
+# merged_names, merged_dirs = [], []
+# for study in studies:
+#     names, dirs = [], []
+#     metafo = heads.read_metadata(study)
+#     print study
+#     for dset in metafo:
+#         if subject is not None and metafo[dset]['subject'] != subject:
+#             continue
+#         bdir = fsdir + '/processed-data/partis/' + study + '/' + label + '/' + dset
+#         if metafo[dset]['timepoint'] == 'merged':
+#             continue
+#         if dset == 'Hs-LN3-5RACE-IgG':  # bad one
+#             continue
+#         if not os.path.exists(bdir):
+#             print '    %s missing' % dset
+#             continue
+#         names.append(metafo[dset]['shorthand'])
+#         dirs.append(bdir + '/plots/' + ptype + '/mute-freqs/overall')
+#     outdir = fsdir + '/dralph/partis/tmp/lots-of-mfreqs/' + study
+#     if subject is not None:
+#         outdir += '/' + subject
+#     check_call(['./bin/compare-plotdirs.py', '--outdir', outdir, '--plotdirs', ':'.join(dirs), '--names', ':'.join(names), '--normalize'])
+#     merged_names += names
+#     merged_dirs += dirs
+# # check_call(['./bin/compare-plotdirs.py', '--outdir', fsdir + '/dralph/partis/tmp/lots-of-mfreqs/merged', '--plotdirs', ':'.join(merged_dirs), '--names', ':'.join(merged_names), '--normalize'])
+# sys.exit()
+
+# # ----------------------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------------------
 def run(cmd_str):
@@ -72,7 +92,7 @@ def cf_nsnps(args, original_glfo):
         return
     v_gene = args.v_genes[0]
     for nsnp in args.nsnp_list:
-        cmd = base_cmd + ' --n-procs 5 --n-tests ' + str(args.n_tests) + ' --n-sim-events 1000' # --slurm'
+        cmd = base_cmd + ' --n-procs 5 --n-tests ' + str(args.n_tests) + ' --n-sim-events 5000 --slurm'
         cmd += ' --sim-v-genes ' + v_gene
         cmd += ' --inf-v-genes ' + v_gene
         cmd += ' --nsnp-list ' + str(nsnp)
