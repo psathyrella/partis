ea024be21f64fe656845156d561023ac37622ebf
duncan ralph <dkralph@gmail.com>
2017-04-27 20:12:59 +0200

    actually looks kinda ok

--- test/plot-gl-set-trees.py
+++ test/plot-gl-set-trees.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+import copy
 import random
 import os
 import tempfile
 import subprocess
 import sys
 sys.path.insert(1, './python')
-from ete3 import ClusterTree
+import ete3
 
 import utils
 
@@ -48,23 +49,73 @@ with open(tree_fname) as treefile:
 
 # make the text matrix for ete
 sorted_all_genes = sorted(all_genes)
-present = {n : [g in gl_sets[n] for g in sorted_all_genes] for n in gls_labels}
 matrix_strs = ['#Names\t' + '\t'.join(gls_labels)]
+# present = {n : [g in gl_sets[n] for g in sorted_all_genes] for n in gls_labels}
+# for igene in range(len(sorted_all_genes)):
+#     line = sorted_all_genes[igene] + '\t' + '\t'.join([str(int(present[label][igene])) for label in gls_labels])
+#     matrix_strs.append(line)
+def color_fcn(gene):
+    if gene in gl_sets['sim'] and gene in gl_sets['inf']:
+        return '0'  # ok
+    elif gene in gl_sets['sim']:
+        return '1'  # missing
+    elif gene in gl_sets['inf']:
+        return '2'  # spurious
+    else:
+        assert False
 for igene in range(len(sorted_all_genes)):
-    line = sorted_all_genes[igene] + '\t' + '\t'.join([str(int(present[label][igene])) for label in gls_labels])
+    line = sorted_all_genes[igene] + '\t' + '\t'.join([color_fcn(sorted_all_genes[igene]) for label in gls_labels])
     matrix_strs.append(line)
 
+
 matrix = '\n'.join(matrix_strs)
-# treestr = "(A:0.7,B:0.7):0.3;"
 # matrix = """
 # #Names\tcol1\tcol2
 # A\t1\t1
 # B\t0\t0
 # """
-print matrix
+# print matrix
+# partis_dir = os.path.dirname(os.path.realpath(__file__)).replace('/test', '')
+# sys.path.insert(1, partis_dir + '/packages/baltic')
+# import treegenerator
+# print treegenerator.get_ascii_tree(treestr.replace('*', '_s').replace('+', '_p_'))
 
-t = ClusterTree(treestr, text_array=matrix)
-t.show("heatmap")
+t = ete3.ClusterTree(treestr) #, text_array=matrix)
+ts = ete3.TreeStyle()
+# treestr = "(A:0.7,B:0.7):0.3;"
+
+scolors = {'ok' : 'DarkSeaGreen', 'missing' : 'IndianRed', 'spurious' : 'IndianRed'}
+faces = {'missing'  : ete3.CircleFace(10, 'white'), 
+         'spurious' : ete3.CircleFace(10, 'black')}
+
+def getstatus(gene):
+    if gene in gl_sets['sim'] and gene in gl_sets['inf']:
+        return 'ok'
+    elif gene in gl_sets['sim']:
+        return 'missing'
+    elif gene in gl_sets['inf']:
+        return 'spurious'
+    else:
+        return 'xxx'
+
+def get_nst(status):
+    nst = ete3.NodeStyle()
+    nst['bgcolor'] = scolors[status]
+    return nst
+
+for n in t.traverse():
+    n.dist = 1
+    if n.is_leaf():
+        status = getstatus(n.name)
+        n.set_style(get_nst(status))
+        if status in faces:
+            print n.name, status
+            n.add_face(copy.deepcopy(faces[status]), column=0)
+
+ts.show_leaf_name = False
+ts.mode = 'c'
+ts.show_scale = False
+t.render('out.png', h=750, tree_style=ts)  # , layout='heatmap'
 
 sys.exit()
 os.remove(aligned_fname)
