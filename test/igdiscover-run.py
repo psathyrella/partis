7575dfd72ddcd428b2ec5449b3195f8a92311e04
duncan ralph <dkralph@gmail.com>
2017-05-04 17:28:36 +0200

    working on igdiscover-test

--- test/igdiscover-run.py
+++ test/igdiscover-run.py
@@ -10,29 +10,23 @@ sys.path.insert(1, './python')
 import utils
 
 # ----------------------------------------------------------------------------------------
-def run_tigger(infname, outfname):
+def run_igdiscover(infname, outfname):
     if utils.output_exists(args, outfname):
         return
 
-    cmd = 'export PATH=%s:$PATH' % args.condapath
-    cmd += 'igdiscover init --db %s/db --reads %s discovertest' % (args.workdir, args.infname)
-    'cp igdiscover-testdata-*/igdiscover.yaml discovertest/'
-    'cd discovertest && igdiscover run'
-
-    cmd = './igblastn'
-    cmd += ' -germline_db_V human_gl_V -germline_db_D human_gl_V -germline_db_J human_gl_J'
-    cmd += ' -auxiliary_data optional_file/human_gl.aux'
-    cmd += ' -domain_system imgt -ig_seqtype Ig -organism human -outfmt \'7 std qseq sseq btop\''
-    cmd += ' -num_threads %d' % args.n_procs
-    cmd += ' -query ' + infname + ' -out ' + outfname
-    
-    cmd = 'cd %s; %s' % (args.igbdir, cmd)
-    utils.simplerun(cmd, shell=True, print_time='igblast')
+    cmds = ['export PATH=%s:$PATH' % args.condapath]
+    cmds += ['export PYTHONNOUSERSITE=True']  # otherwise it finds the pip-installed packages in .local and breaks (see https://github.com/conda/conda/issues/448)
+    cmds += ['igdiscover init --db %s/db --reads %s igdwork' % (args.workdir, args.infname)]
+    cmds += ['cp %s igdwork/' % args.yamlfname]
+    cmds += ['cd %s/' % args.workdir + '/igdwork']
+    cmds += ['igdiscover run']
+    utils.simplerun(';\n'.join(cmds), shell=True, print_time='igdiscover')
 
 # ----------------------------------------------------------------------------------------
 parser = argparse.ArgumentParser()
 parser.add_argument('--infname', required=True)
 parser.add_argument('--outfname', required=True)
+parser.add_argument('--yamlfname', default='test/igdiscover.yaml')
 parser.add_argument('--workdir', required=True)
 # parser.add_argument('--n-procs', default=1, type=int)
 parser.add_argument('--overwrite', action='store_true')
@@ -48,6 +42,4 @@ outdir = os.path.dirname(args.outfname)  # kind of annoying having <args.workdir
 # utils.prep_dir(args.workdir, wildlings=['*.cmd']) #'*.fmt7'])
 # utils.prep_dir(outdir, allow_other_files=True)
 
-# run_igblast(args.infname, igblast_outfname)
-# run_changeo(args.infname, igblast_outfname, changeo_outfname)
-# run_tigger(changeo_outfname, args.outfname)
+run_igdiscover(args.infname, args.outfname)
