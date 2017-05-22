37eb477fba92428112c94d2b20c1e243c4b23c9c
Duncan Ralph <dkralph@gmail.com>
2017-05-22 14:10:30 -0700

    add timing info

--- python/alleleclusterer.py
+++ python/alleleclusterer.py
@@ -1,3 +1,4 @@
+import time
 import sys
 import os
 
@@ -15,7 +16,11 @@ class AlleleClusterer(object):
     def get_alleles(self, swfo, glfo, debug=True):
         qr_seqs = {query : swfo[query][self.region + '_qr_seqs'][0] for query in swfo['queries']}
         consensus_fname = self.args.workdir + '/cluster-consensus-seqs.fa'
-        partition = utils.run_vsearch(qr_seqs, self.args.workdir + '/vsearch', threshold=swfo['mute-freqs'][self.region], n_procs=self.args.n_procs, consensus_fname=consensus_fname)
+        threshold = swfo['mute-freqs'][self.region]
+        print '  running vsearch with threshold %.2f (*300 = %d)' % (threshold, int(threshold * 300))
+        start = time.time()
+        partition = utils.run_vsearch(qr_seqs, self.args.workdir + '/vsearch', threshold=threshold, n_procs=self.args.n_procs, consensus_fname=consensus_fname)
+        print '  vsearch: %.1f sec' % (time.time()-start)
 
         # read vsearch output (and convert consensus file to a more useful format)
         consensus_seqs = utils.read_fastx(consensus_fname)
@@ -44,6 +49,7 @@ class AlleleClusterer(object):
                 for gene, glseq in glseqs.items():
                     print '  %-12s   %s' % (utils.color_gene(gene, width=12), utils.color_mutants(cinfo['cons_seq'], glseq, print_isnps=True, align=True))
 
+            newname = hash(cinfo['cons_seq'])
             final_alleles[newname] = cinfo['cons_seq']
 
-        return xxx #[cinfo[ consensus_info]
+        return final_alleles
