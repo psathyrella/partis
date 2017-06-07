6f5dc8628b5a764ae42bfaa34f5a4f72d5c457d6
Duncan Ralph <dkralph@gmail.com>
2017-06-07 13:40:27 -0700

    add clonal collapse and j mutation threshold to alleleclusterer

--- python/alleleclusterer.py
+++ python/alleleclusterer.py
@@ -16,17 +16,26 @@ class AlleleClusterer(object):
         self.args = args
         self.region = 'v'
         self.absolute_n_seqs_min = 15
+        self.max_j_mutations = 8
 
     # ----------------------------------------------------------------------------------------
-    def get_alleles(self, queryfo, threshold, glfo, swfo=None, debug=True):
+    def get_alleles(self, queryfo, threshold, glfo, swfo=None, reco_info=None, debug=True):
         # NOTE do *not* modify <glfo> (in the future it would be nice to just modify <glfo>, but for now we need it to be super clear in partitiondriver what is happening to <glfo>)
         if swfo is None:
+            print '  note: not collapsing clones, since we\'re working from vsearch v-only info'
             qr_seqs = {q : qfo['qr_seq'] for q, qfo in queryfo.items()}
             gene_info = {q : qfo['gene'] for q, qfo in queryfo.items()}
         else:
             assert queryfo is None
-            qr_seqs = {query : indelutils.get_qr_seqs_with_indels_reinstated(swfo[query], iseq=0)[self.region] for query in swfo['queries']}
-            gene_info = {q : swfo[q][self.region + '_gene'] for q in swfo['queries']}
+            clusters = utils.collapse_naive_seqs(swfo)
+            qr_seqs = {}
+            for cluster in clusters:  # take the sequence with the lowest j mutation for each cluster, if it doesn't have too many j mutations NOTE choose_cluster_representatives() in allelefinder is somewhat similar
+                j_mutations = {q : utils.get_n_muted(swfo[q], iseq=0, restrict_to_region='j') for q in cluster}
+                best_query, smallest_j_mutations = sorted(j_mutations.items(), key=operator.itemgetter(1))[0]
+                if smallest_j_mutations < self.max_j_mutations:
+                    qr_seqs[best_query] = indelutils.get_qr_seqs_with_indels_reinstated(swfo[best_query], iseq=0)[self.region]
+            print '    collapsed %d input sequences into %d representatives from %d clones' % (len(swfo['queries']), len(qr_seqs), len(clusters))
+            gene_info = {q : swfo[q][self.region + '_gene'] for q in qr_seqs}
 
         msa_fname = self.args.workdir + '/msa.fa'
         print '  running vsearch with threshold %.2f (*300 = %d)' % (threshold, int(threshold * 300))
@@ -53,22 +62,36 @@ class AlleleClusterer(object):
             # dot_products = [utils.dot_product(clusterfo['cons_seq'], seq1, seq2) for seq1, seq2 in itertools.combinations([seqfo['seq'] for seqfo in clusterfo['seqfos']], 2)]
             # mean_dot_product = numpy.average(dot_products)
 
-            glcounts = {}
+            glcounts, true_glcounts = {}, {}
             for seqfo in clusterfo['seqfos']:
                 gene = gene_info[seqfo['name']]
                 if gene not in glcounts:
                     glcounts[gene] = 0
                 glcounts[gene] += 1
+                if reco_info is not None:
+                    gene = reco_info[seqfo['name']][self.region + '_gene']
+                    if gene not in true_glcounts:
+                        true_glcounts[gene] = 0
+                    true_glcounts[gene] += 1
             sorted_glcounts = sorted(glcounts.items(), key=operator.itemgetter(1), reverse=True)
+            if reco_info is not None:
+                true_sorted_glcounts = sorted(true_glcounts.items(), key=operator.itemgetter(1), reverse=True)
             if debug:
                 print '%d seqs' % len(clusterfo['seqfos'])
                 print '    %-12s  %4s   %s' % ('consensus', '', clusterfo['cons_seq'])
                 for gene, counts in sorted_glcounts:
                     print '    %-12s  %4d   %s' % (utils.color_gene(gene, width=12), counts, utils.color_mutants(clusterfo['cons_seq'], glfo['seqs'][self.region][gene], print_isnps=True, align=True))
+                if reco_info is not None:
+                    mean_j_mutation = numpy.mean([utils.get_n_muted(reco_info[seqfo['name']], iseq=0, restrict_to_region='j') for seqfo in clusterfo['seqfos']])
+                    print '  %s   %d' % (utils.color('green', 'true'), mean_j_mutation)
+                    #':'.join([seqfo['name'] for seqfo in clusterfo['seqfos']]))
+                    print '    %-12s  %4s   %s' % ('consensus', '', clusterfo['cons_seq'])
+                    for gene, counts in true_sorted_glcounts:
+                        print '    %-12s  %4d   %s' % (utils.color_gene(gene, width=12), counts, utils.color_mutants(clusterfo['cons_seq'], glfo['seqs'][self.region][gene], print_isnps=True, align=True))
 
-            # choose some existing gene to use as a template (the most similar gene might be a better choice, but deciding on "most similar" would involve adjudicating between snps and indels, and it shouldn't really matter)
+            # choose the most common existing gene to use as a template (the most similar gene might be a better choice, but deciding on "most similar" would involve adjudicating between snps and indels, and it shouldn't really matter)
             template_gene, _ = sorted_glcounts[0]
-            template_seq = glfo['seqs'][self.region][template_gene]  # not necessarily the most similar (but it probably is)
+            template_seq = glfo['seqs'][self.region][template_gene]
             template_cpos = utils.cdn_pos(glfo, self.region, template_gene)
 
             new_name = template_gene + '+' + ''.join(numpy.random.choice(list('xyz'), size=8))  # er, not sure what to use here, but at least this probably won't result in collisions
