b8895c0054d6d4e5fab87fa76328498a9c1247f4
Duncan Ralph <dkralph@gmail.com>
2017-06-25 12:01:54 -0700

    go back to single-process hmm writing
    seemed to hang on large samples. maybe memory duplication is the problem?

--- python/partitiondriver.py
+++ python/partitiondriver.py
@@ -1105,13 +1105,17 @@ class PartitionDriver(object):
         if self.args.debug:
             print '    to %s' % parameter_dir + '/hmms'
 
-        def write_single_hmm(gene):
-            writer = HmmWriter(parameter_dir, hmm_dir, gene, self.glfo, self.args)
-            writer.write()
+        # def write_single_hmm(gene):
+        #     writer = HmmWriter(parameter_dir, hmm_dir, gene, self.glfo, self.args)
+        #     writer.write()
 
-        procs = [multiprocessing.Process(target=write_single_hmm, args=(gene,))
-                 for region in utils.regions for gene in self.glfo['seqs'][region]]
-        utils.run_proc_functions(procs, self.args.n_procs)
+        # procs = [multiprocessing.Process(target=write_single_hmm, args=(gene,))
+        #          for region in utils.regions for gene in self.glfo['seqs'][region]]
+        # utils.run_proc_functions(procs, self.args.n_procs)
+        for region in utils.regions:
+            for gene in self.glfo['seqs'][region]:
+                writer = HmmWriter(parameter_dir, hmm_dir, gene, self.glfo, self.args)
+                writer.write()
 
         print '(%.1f sec)' % (time.time()-start)
 
