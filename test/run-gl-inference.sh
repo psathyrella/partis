d7d84ff002d3c4233844d7cc585da7c34eb1255d
Duncan Ralph <dkralph@gmail.com>
2017-07-31 11:03:22 -0700

    fiddle with run-gl-inference

--- test/run-gl-inference.sh
+++ test/run-gl-inference.sh
@@ -9,10 +9,18 @@ testopts="--n-tests 1 --n-procs-per-test 2 --no-slurm"
 
 
 glscmd="$cmd gls-gen --label $label"
-for diff in easy hard; do
-    for meth in simu tigger-default igdiscover; do #partis full tigger-default tigger-tuned igdiscover; do  # NOTE can add all methods to --methods arg now
-	$glscmd --methods $meth --n-tests 3 --n-procs-per-test 10 --gls-gen-difficulty easy --no-slurm  #--dry  # --plot
+# for diff in hard; do
+#     for meth in simu tigger-default igdiscover; do #partis full tigger-default tigger-tuned igdiscover; do  # NOTE can add all methods to --methods arg now
+# 	$glscmd --methods $meth --n-tests 3 --n-procs-per-test 10 --gls-gen-difficulty $diff --no-slurm  #--dry  # --plot
+#     done
+# done
+
+for diff in hard; do
+    for meth in partis; do #partis full tigger-default igdiscover; do  # NOTE can add all methods to --methods arg now, i just keep 'em separate here so the log files are separate
+	echo $glscmd --methods $meth --n-tests 3 --iteststart 0 --n-procs-per-test 20 --gls-gen-difficulty $diff --plot #--dry  # --plot
     done
 done
+
+
 # $cmd data --label $label $testopts --n-random-queries 5000  # laptop
 # $cmd data --label $label --n-procs-per-test 15
