286da056463e025d9b82038d25e72250263bd305
Duncan Ralph <dkralph@gmail.com>
2017-08-11 13:08:14 -0700

    improve example-output-processing

--- bin/example-output-processing.py
+++ bin/example-output-processing.py
@@ -17,12 +17,15 @@ with open(partis_path + '/test/reference-results/annotate-new-simu.csv') as csvf
     for line in reader:
         if line['v_gene'] == '':  # failed (i.e. couldn't find an annotation)
             continue
-        utils.process_input_line(line)
-        utils.add_implicit_info(glfo, line)
-        utils.print_reco_event(line)
+        utils.process_input_line(line)  # converts strings in the csv file to floats/ints/dicts/etc.
+        utils.add_implicit_info(glfo, line)  # add stuff to <line> that's useful, isn't written to the csv since it's redundant
+        utils.print_reco_event(line)  # print ascii-art representation of the rearrangement event
+        print '\navailable annotation info for each line (see manual for descriptions):'
+        for key, val in line.items():
+            print '%20s %s' % (key, val)
         break
 
-print 'then parse a partition csv file:'
+print '\n\nthen parse a partition csv file:'
 cp = ClusterPath()
 cp.readfile(partis_path + '/test/reference-results/seed-partition-new-simu.csv')
 cp.print_partitions(abbreviate=True)
