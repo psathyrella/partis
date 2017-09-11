26f787fec42ebf33a9e802a74a171225e0434245
duncan ralph <dkralph@gmail.com>
2017-09-11 14:05:43 -0700

    generalize plot-gl-set-trees
    holy crap that was a disaster

--- bin/plot-gl-set-trees.py
+++ bin/plot-gl-set-trees.py
@@ -19,31 +19,70 @@ import glutils
 sys.path.insert(1, './datascripts')
 import heads
 
+primary_colors = OrderedDict((
+    ('red', '#d77c7c'),
+    ('yellow', '#e3e317'),
+    # ('green', '#85ad98'),
+    ('blue', '#94a3d1'),
+    # 'LightSteelBlue',
+))
+
+combo_colors = {  # colors need to sorted before calling '-'.join()
+    'red-&-yellow' : '#f59f49',
+    'blue-&-yellow' : '#85ad98',  # green above ^
+    'blue-&-red' : '#e26ade',
+}
+
+all_colors = dict(primary_colors.items() + combo_colors.items())
+all_colors['LightGrey'] = 'LightGrey'
+
+# ----------------------------------------------------------------------------------------
+def pairkey(name1, name2):
+    return '-&-'.join(sorted([name1, name2]))
+
+# ----------------------------------------------------------------------------------------
+def get_color_name(code):
+    names = [n for n, c in all_colors.items() if c == code]
+    if len(names) != 1:
+        # print 'couldn\'t find a unique color name for %s (found %s)' % (code, names)
+        return code
+    return names[0]
+
 scolors = {
     'ok' : 'DarkSeaGreen',
-    'missing' : '#d77c7c',  #'IndianRed',
-    'spurious' : '#a44949',  #'IndianRed',
+    'missing' : '#d77c7c',
+    'spurious' : '#a44949',
     'data' : 'LightSteelBlue',
-    'both' : 'LightGrey',
+    'all' : 'LightGrey',
 }
-metafos = heads.read_metadata('kate-qrs')
-for ds in metafos:
-    if 'LN1' in ds or 'LN2' in ds:
-        scolors[ds] = '#85ad98'  # green
-    elif 'LN4' in ds or 'LN3' in ds:
-        scolors[ds] = '#94a3d1'  # blue
-metafos = heads.read_metadata('sheng-gssp')
-for ds in metafos:
-    if '-m-' in ds:
-        scolors[ds] = '#85ad98'  # green
-    elif '-g-' in ds:
-        scolors[ds] = '#94a3d1'  # blue
+
 # faces = {}
 # faces = { 'missing' : ete3.AttrFace("name", fsize=30)}
 #         # faces.add_face_to_node(N, node, 0, position="aligned")}
 # faces = {'missing'  : ete3.CircleFace(10, 'white'),
 #          'spurious' : ete3.CircleFace(10, 'black')}
 
+# ----------------------------------------------------------------------------------------
+def set_colors(gl_sets, ref_label=None):
+    if ref_label is not None:
+        return
+
+    if len(gl_sets) == 1:  # single-sample data
+        scolors[gl_sets.keys()[0]] = scolors['data']
+        return
+
+    names = gl_sets.keys()
+    if len(names) > 3:
+        raise Exception('need more colors')
+
+    pclist = primary_colors.keys()
+    pccodes = primary_colors.values()
+    for iname in range(len(names)):
+        scolors[names[iname]] = pccodes[iname]  # set this data set to a primary color
+        for jname in range(iname + 1, len(names)):
+            scolors[pairkey(names[iname], names[jname])] = combo_colors[pairkey(pclist[iname], pclist[jname])]  # ...and set its combos with other data sets to the apropriate secondary color
+
+# ----------------------------------------------------------------------------------------
 def get_cmdfos(cmdstr, workdir, outfname):
     return [{'cmd_str' : cmdstr,
              'workdir' : workdir,
@@ -93,66 +132,35 @@ def make_tree(all_genes, workdir, use_cache=False):
     return treefname
 
 # ----------------------------------------------------------------------------------------
-def getstatus(gl_sets, node, ref_label=None):
+def getstatus(gene_categories, node, ref_label=None, debug=False):
     gene = node.name
 
-    # simulation
-    if ref_label is not None:
-        if not node.is_leaf():
-            return 'internal'
-        elif gene in gl_sets['sim'] and gene in gl_sets['inf']:
-            return 'ok'
-        elif gene in gl_sets['sim']:
-            return 'missing'
-        elif gene in gl_sets['inf']:
-            return 'spurious'
-        else:
-            raise Exception('couldn\'t decide on status for node with name %s' % gene)
-
-    if len(gl_sets) == 1:
-        return 'leaf' if node.is_leaf() else 'internal'
-
-    foundlist = [ds for ds in gl_sets if gene in gl_sets[ds]]  # list of datasets that have this gene
     if not node.is_leaf():
         return 'internal'
-    elif len(foundlist) == len(gl_sets):  # if this node/gene is in every gl set
-        return 'both'  # should change to 'all' now
-    else:
-        assert len(foundlist) > 0
-        if len(foundlist) > 1:
-            raise Exception('update!')
-        return foundlist[0]
+    cats = [cat for cat, genes in gene_categories.items() if gene in genes]
+    if len(cats) != 1:
+        print cats
+        raise Exception()
+    if debug:
+        print '%-50s   %s' % (gene, cats[0])
+    return cats[0]
 
 # ----------------------------------------------------------------------------------------
-def print_results(gl_sets, ref_label=None):
-    tmpfo = None
-    for ds_1, ds_2 in itertools.combinations(gl_sets, 2):
-        tmpfo = OrderedDict(((ds_1, set(gl_sets[ds_1]) - set(gl_sets[ds_2])),
-                             (ds_2, set(gl_sets[ds_2]) - set(gl_sets[ds_1])),
-                             ('both', set(gl_sets[ds_2]) & set(gl_sets[ds_1]))))
-        if ref_label is not None:
-            assert ref_label == ds_1 or ref_label == ds_2
-            inf_label = ds_1 if ds_1 != ref_label else ds_2
-            tmpfo['missing'] = tmpfo[ref_label]
-            tmpfo['spurious'] = tmpfo[inf_label]
-            tmpfo['ok'] = tmpfo['both']
-            del tmpfo[ref_label]
-            del tmpfo[inf_label]
-            del tmpfo['both']
-    if tmpfo is None:  # only one of 'em
-        tmpfo = {n : s for n, s in gl_sets.items()}
-
-    pwidth = str(max([len(n) for n in tmpfo]))
-    for name, genes in tmpfo.items():
-        genestr = ' '.join([utils.color_gene(g) for g in genes])
+def print_results(gene_categories, ref_label=None):
+    pwidth = str(max([len(n) for n in gene_categories]))
+    for name, genes in gene_categories.items():
+        if name not in scolors:
+            raise Exception('status \'%s\' not in scolors' % name)
         if name == 'ok':
-            print ('       %2d %' + pwidth + 's %s') % (len(genes), name, genestr)
+            genestr = ''
         else:
-            print ('    %-' + pwidth + 's') % name,
-            if len(genes) == 0:
-                print ' %s' % utils.color('blue', 'none')
-            else:
-                print ' %2d    %s' % (len(genes), genestr)
+            genestr = ' '.join([utils.color_gene(g) for g in genes])
+        print ('    %-' + pwidth + 's') % name,
+        print '%20s' % get_color_name(scolors[name]),
+        if len(genes) == 0:
+            print ' %s' % utils.color('blue', 'none')
+        else:
+            print ' %2d    %s' % (len(genes), genestr)
 
 # ----------------------------------------------------------------------------------------
 def get_gene_sets(glsfnames, glslabels, ref_label=None, classification_fcn=None, debug=False):
@@ -175,22 +183,57 @@ def get_gene_sets(glsfnames, glslabels, ref_label=None, classification_fcn=None,
         gl_sets = {pv : {label : {g : gl_sets[label][g] for g in gl_sets[label] if classification_fcn(g) == pv} for label in gl_sets} for pv in all_primary_versions}
         all_genes = {pv : {g : s for g, s in all_genes.items() if classification_fcn(g) == pv} for pv in all_primary_versions}
 
-    return all_genes, gl_sets
+    if len(gl_sets) > 3:
+        raise Exception('not implemented')
+    gcats = OrderedDict()
+
+    gcats['all'] = set()
+    if len(gl_sets) > 2:
+        gcats['all'] = set(all_genes)  # gcats['all'] is genes that are in *every* gl set, whereas <all_genes> is genes that're in *any* of 'em (i know, i know...)
+        for genes in gl_sets.values():
+            gcats['all'] &= set(genes)
+
+    for name, genes in gl_sets.items():
+        gcats[name] = set(genes) - gcats['all']
+
+    for ds_1, ds_2 in itertools.combinations(gl_sets, 2):
+        gcats[ds_1] -= set(gl_sets[ds_2])
+        gcats[ds_2] -= set(gl_sets[ds_1])
+        gcats[pairkey(ds_1, ds_2)] = set(gl_sets[ds_2]) & set(gl_sets[ds_1]) - gcats['all']
+
+    if ref_label is not None:
+        assert len(gl_sets) == 2
+        assert ref_label in gl_sets
+        inf_label = [ds for ds in gl_sets if ds != ref_label][0]
+        gcats['missing'] = gcats[ref_label]
+        gcats['spurious'] = gcats[inf_label]
+        gcats['ok'] = gcats[pairkey(ref_label, inf_label)]
+        del gcats[ref_label]
+        del gcats[inf_label]
+        del gcats[pairkey(ref_label, inf_label)]
+
+    any_check = []
+    for key, genes in gcats.items():
+        any_check += genes
+    if sorted(any_check) != sorted(all_genes):
+        raise Exception('you done messed up')
+
+    if len(gl_sets) <= 2:
+        del gcats['all']
+
+    return all_genes, gl_sets, gcats
 
 # ----------------------------------------------------------------------------------------
 def set_node_style(node, status, ref_label=None, pair=False):
     linewidth = 2
-    if ref_label is None:
-        if pair:
-            if status != 'internal':
-                node.img_style['bgcolor'] = scolors[status]
-        else:
-            node.img_style['bgcolor'] = scolors['data']
-    elif status != 'internal':
+
+    if status != 'internal':
+        if status not in scolors:
+            raise Exception('status \'%s\' not in scolors' % status)
         node.img_style['bgcolor'] = scolors[status]
 
-    if status != 'internal' and glutils.is_novel(node.name):
-        node.add_face(ete3.CircleFace(2.5, 'Gold'), column=0) #, position='aligned')
+        if glutils.is_novel(node.name):
+            node.add_face(ete3.CircleFace(2.5, 'Gold'), column=0) #, position='aligned')
 
     node.img_style['hz_line_width'] = linewidth
     node.img_style['vt_line_width'] = linewidth
@@ -237,7 +280,7 @@ def set_distance_to_zero(node, debug=False):
     return descendents == entirety_of_gene_family
 
 # ----------------------------------------------------------------------------------------
-def draw_tree(plotdir, plotname, treestr, gl_sets, all_genes, ref_label=None, arc_start=None, arc_span=None):
+def draw_tree(plotdir, plotname, treestr, gl_sets, all_genes, gene_categories, ref_label=None, arc_start=None, arc_span=None):
     etree = ete3.ClusterTree(treestr)
     node_names = set()  # make sure we get out all the genes we put in
     for node in etree.traverse():
@@ -246,7 +289,7 @@ def draw_tree(plotdir, plotname, treestr, gl_sets, all_genes, ref_label=None, ar
             node.dist = 0.
         # if '+' in node.name and node.dist > 0.1:  # dammit, I should have named those differently...
         #     node.dist = 0.05
-        status = getstatus(gl_sets, node, ref_label=ref_label)
+        status = getstatus(gene_categories, node, ref_label=ref_label)
         set_node_style(node, status, ref_label=ref_label, pair=(len(gl_sets) > 1))
         if node.is_leaf():
             node_names.add(node.name)
@@ -270,14 +313,15 @@ def draw_tree(plotdir, plotname, treestr, gl_sets, all_genes, ref_label=None, ar
 
 # ----------------------------------------------------------------------------------------
 def plot_trees(args, plotdir, plotname, glsfnames, glslabels, leg_title=None, title=None):
-    all_genes, gl_sets = get_gene_sets(glsfnames, glslabels, ref_label=args.ref_label)
-    print_results(gl_sets, ref_label=args.ref_label)
+    all_genes, gl_sets, gene_categories = get_gene_sets(glsfnames, glslabels, ref_label=args.ref_label)
+    set_colors(gl_sets, ref_label=args.ref_label)
+    print_results(gene_categories, ref_label=args.ref_label)
 
     treefname = make_tree(all_genes, plotdir + '/workdir', use_cache=args.use_cache)
     with open(treefname) as treefile:
         treestr = treefile.read().strip()
 
-    draw_tree(plotdir, plotname, treestr, gl_sets, all_genes, ref_label=args.ref_label)
+    draw_tree(plotdir, plotname, treestr, gl_sets, all_genes, gene_categories, ref_label=args.ref_label)
 
 # ----------------------------------------------------------------------------------------
 parser = argparse.ArgumentParser()
