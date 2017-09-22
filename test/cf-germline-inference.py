e687b389f6fd0d7821f07bcd37f6db9d476075e3
duncan ralph <dkralph@gmail.com>
2017-09-22 14:07:57 -0700

    fiddle with jason-influenza cfg

--- test/cf-germline-inference.py
+++ test/cf-germline-inference.py
@@ -256,7 +256,8 @@ def plot_tests(args, baseoutdir, method, method_vs_method=False):
                 dsetfos.remove([study, ds_1])
                 dsetfos.remove([study, ds_2])
             for study, dset in dsetfos:
-                get_data_plots(args, baseoutdir, [method], study, [dset])
+                print 'hmmmm %s' % dset  # crashes below since both method and dset lists are of length one
+                # get_data_plots(args, baseoutdir, [method], study, [dset])
     else:
         plot_single_test(args, baseoutdir, method)
 
@@ -396,7 +397,7 @@ default_varvals = {
         # 'kate-qrs' : ['1g', '4g', '1k', '1l', '4k', '4l'],
         # 'laura-mb-2' : ['BF520-m-W1', 'BF520-m-M9', 'BF520-g-W1', 'BF520-g-M9'], #, 'BF520-k-W1', 'BF520-l-W1', 'BF520-k-M9', 'BF520-l-M9']
         # 'jason-influenza' : ['FV-igh-m2d', 'FV-igh-m1h', 'FV-igh-p1h', 'FV-igh-p1d', 'FV-igh-p3d', 'FV-igh-p7d', 'FV-igh-p14d', 'FV-igh-p21d', 'FV-igh-p28d', 'GMC-igh-m8d', 'GMC-igh-m2d', 'GMC-igh-m1h', 'GMC-igh-p1h', 'GMC-igh-p1d', 'GMC-igh-p3d', 'GMC-igh-p7d', 'GMC-igh-p14d', 'GMC-igh-p21d', 'GMC-igh-p28d', 'IB-igh-m8d', 'IB-igh-m2d', 'IB-igh-m1h', 'IB-igh-p1h', 'IB-igh-p1d', 'IB-igh-p3d', 'IB-igh-p7d', 'IB-igh-p14d', 'IB-igh-p21d', 'IB-igh-p28d'],
-        # 'jason-influenza' : ['FV-igh', 'GMC-igh', 'IV-igh'],
+        # 'jason-influenza' : ['FV-igh', 'GMC-igh', 'IB-igh'],
     }
 }
 all_data_pairs = {
@@ -414,6 +415,23 @@ all_data_pairs = {
     'sheng-gssp' : [
         ['lp23810-m-pool', 'lp23810-g-pool'],
         ['lp08248-m-pool', 'lp08248-g-pool'],
+    ],
+    'jason-influenza' : [
+        ['FV-igh-m2d', 'FV-igh-p28d'],
+        ['FV-igh-m1h', 'FV-igh-p21d'],
+        ['FV-igh-p1h', 'FV-igh-p14d'],
+        ['FV-igh-p1d', 'FV-igh-p7d'],
+        # ['FV-igh-p3d', '],  # not sure what to do with the odd one out
+        ['GMC-igh-m8d', 'GMC-igh-p28d'],
+        ['GMC-igh-m2d', 'GMC-igh-p21d'],
+        ['GMC-igh-m1h', 'GMC-igh-p14d'],
+        ['GMC-igh-p1h', 'GMC-igh-p7d'],
+        ['GMC-igh-p1d', 'GMC-igh-p3d'],
+        ['IB-igh-m8d', 'IB-igh-p28d'],
+        ['IB-igh-m2d', 'IB-igh-p21d'],
+        ['IB-igh-m1h', 'IB-igh-p14d'],
+        ['IB-igh-p1h', 'IB-igh-p7d'],
+        ['IB-igh-p1d', 'IB-igh-p3d'],
     ],
 }
 default_varvals['data'] = ':'.join([study + '/' + heads.full_dataset(heads.read_metadata(study), dset) for study in default_varvals['data'] for dset in default_varvals['data'][study]])
