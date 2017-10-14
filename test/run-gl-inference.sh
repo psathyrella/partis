d4483dffa15f0f70a0e111997803a1a14f6195c3
Duncan Ralph <dkralph@gmail.com>
2017-10-13 19:36:55 -0700

    update test/run-gl-inference

--- test/run-gl-inference.sh
+++ test/run-gl-inference.sh
@@ -1,23 +1,26 @@
 cmd=./test/cf-germline-inference.py
 testopts="--n-tests 1 --n-procs-per-test 2 --no-slurm"
 
-# label=XXX
+# varvals
+# label=XXX  # not sure what it was
 # for tname in alcluster; do #mfreq nsnp multi-nsnp prevalence n-leaves weibull alcluster; do
 #     # $cmd $tname --label $label $testopts  # laptop
 #     $cmd $tname --n-tests 10 --label $label # --plot
 # done
 
-# label=v9
+# # gls-gen
+# label=v10
 # glscmd="$cmd gls-gen --label $label"
 # for diff in easy hard; do
-#     for meth in full; do #simu partis full tigger-default igdiscover; do  # NOTE can add all methods to --methods arg now, i just keep 'em separate here so the log files are separate
-# 	# for itest in {0..2}; do
-# 	#     $glscmd --methods $meth --n-tests $((itest + 1)) --iteststart $itest --n-procs-per-test 12 --gls-gen-difficulty $diff --no-slurm  # --plot
-# 	# done
-# 	$glscmd --methods $meth --n-tests 3 --n-procs-per-test 20 --gls-gen-difficulty $diff --plot --plotcache
+#     for meth in full partis; do #simu partis full tigger-default igdiscover; do  # NOTE can add all methods to --methods arg now, i just keep 'em separate here so the log files are separate
+# 	for itest in {3..10}; do
+# 	    $glscmd --methods $meth --n-tests $((itest + 1)) --iteststart $itest --n-procs-per-test 10 --gls-gen-difficulty $diff #--no-slurm  # --plot
+# 	done
+# 	# $glscmd --methods $meth --n-tests 3 --n-procs-per-test 10 --gls-gen-difficulty $diff --plot # --plotcache
 #     done
 # done
 
+# data
 # label=v10
 # $cmd data --label $label $testopts --n-random-queries 5000  # laptop
 # $cmd data --label $label --n-procs-per-test 15 --dry
