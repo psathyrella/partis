23363531e4ae0c872d357805914e20cb06e907d6
duncan ralph <dkralph@gmail.com>
2017-12-22 14:58:48 -0800

    remove unused is_boundary thing

--- python/hmmwriter.py
+++ python/hmmwriter.py
@@ -178,7 +178,7 @@ class HmmWriter(object):
         self.debug = debug
         self.codon_positions = {r : glfo[c + '-positions'] for r, c in utils.conserved_codons[args.locus].items()}
 
-        print self.raw_name
+        print self.raw_name  # XXX
         # parameters with values that I more or less made up
         self.precision = '16'  # number of digits after the decimal for probabilities
         self.eps = 1e-6  # NOTE I also have an eps defined in utils, and they should in principle be combined
@@ -707,14 +707,7 @@ class HmmWriter(object):
             if germline_nuke in utils.ambiguous_bases:
                 prob = 1. / len(utils.nukes)
             else:
-                # if this is the leftmost base in d or j, or if it's rightmost in v or d, use the overall mean mute freq (because we don't really have a handle for inferring it)
-                is_boundary_base = False  # UPDATE doesn't seem to help at all
-                # if inuke == 0 and (self.region == 'd' or self.region == 'j'):
-                #     is_boundary_base = True
-                # if inuke == len(self.germline_seq) - 1 and (self.region == 'v' or self.region == 'd'):
-                #     is_boundary_base = True
-
-                if not is_boundary_base and inuke in self.mute_freqs:  # if we found this base in this gene version in the data parameter file
+                if inuke in self.mute_freqs:  # if we found this base in this gene version in the data parameter file
                     mute_freq = self.mute_freqs[inuke]
                 else:
                     mute_freq = self.mute_freqs['overall_mean']
