e3023df36fa58130314bb5f1432ffd9d3cc418f3
duncan ralph <dkralph@gmail.com>
2018-01-29 17:29:42 -0800

    move sklearn mds to python/mds.py

--- python/mds.py
+++ python/mds.py
@@ -2,6 +2,12 @@ import sys
 import os
 import string
 from matplotlib import pyplot as plt
+import scipy
+import numpy
+from sklearn import manifold
+from sklearn.metrics import euclidean_distances
+# from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 
 import utils
 
@@ -188,3 +194,45 @@ def kmeans_cluster(n_clusters, seqfos, all_qr_seqs, base_workdir, seed, reco_inf
 # ----------------------------------------------------------------------------------------
 
     return clusterfos
+
+# ----------------------------------------------------------------------------------------
+def run_sklearn_mds(seqfos, n_components, n_clusters, seed, aligned=False, max_iter=1000, eps=1e-9, n_jobs=-1):
+    print 'align'
+    if not aligned:
+        seqfos = utils.align_many_seqs(seqfos)
+
+    print '  distances'
+    # translations = string.maketrans('ACGT-', '01234')
+    # def convert(seq):
+    #     return [int(c) for c in seq.translate(translations)]
+    # converted_seqs = [convert(x['seq']) for x in seqfos]
+    # similarities = scipy.spatial.distance.pdist(converted_seqs, 'hamming')
+    # similarities = scipy.spatial.distance.squareform(similarities)
+    similarities = scipy.spatial.distance.squareform([utils.hamming_fraction(seqfos[i]['seq'], seqfos[j]['seq']) for i in range(len(seqfos)) for j in range(i + 1, len(seqfos))])
+
+    print '  mds'
+    random_state = numpy.random.RandomState(seed=seed)
+    mds = manifold.MDS(n_components=n_components, max_iter=max_iter, eps=eps, random_state=random_state, dissimilarity="precomputed", n_jobs=n_jobs)
+    pos = mds.fit_transform(similarities)  # hm, should this be mds.fit(similarities).embedding_?
+
+    print '  kmeans'
+    kmeans = KMeans(n_clusters=n_clusters, random_state=random_state).fit(pos)
+
+    print '  plot'
+    if n_components % 2 != 0:
+        print '%s odd number of components' % utils.color('red', 'warning')
+
+    def plot_component_pair(ipair, plotname):
+        colors = ['forestgreen', 'red', 'blue', 'black', 'yellow', 'purple', 'orange']
+        fig = plt.figure(1)
+        ax = plt.axes([0., 0., 1., 1.])
+        for iseq in range(len(seqfos)):
+            plt.scatter(pos[iseq][ipair], pos[iseq][ipair + 1], color=colors[kmeans.labels_[iseq]])
+        # plt.scatter(pos[:, 0], pos[:, 1], color='forestgreen', lw=0, label='MDS')
+        # plt.legend(scatterpoints=1, loc='best', shadow=False)
+        plt.savefig(plotname)
+
+    for ipair in range(0, n_components - 1, 2):
+        print '  %d' % ipair
+        plot_component_pair(ipair, 'tmp-%d.svg' % ipair)
+
