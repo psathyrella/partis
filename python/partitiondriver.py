09c2f4c71573bec2f59bb80a7de27492a7032206
duncan ralph <dkralph@gmail.com>
2018-05-22 09:28:18 -0700

    update alleleremover call in partitiondriver

--- python/partitiondriver.py
+++ python/partitiondriver.py
@@ -212,11 +212,11 @@ class PartitionDriver(object):
     def cache_parameters(self):
         print 'caching parameters'
 
-        # remove unlikely alleles
+        # remove unlikely alleles (can only remove v alleles here, since we're using vsearch annotations, but that's ok since it's mostly a speed optimization)
         if not self.args.dont_remove_unlikely_alleles:
             self.set_vsearch_info()
             alremover = AlleleRemover(self.glfo, self.args)
-            alremover.finalize(sorted(self.vs_info['gene-counts'].items(), key=operator.itemgetter(1), reverse=True), debug=self.args.debug_allele_finding)
+            alremover.finalize({'v' : self.vs_info['gene-counts']}, debug=self.args.debug_allele_finding)
             glutils.remove_genes(self.glfo, alremover.genes_to_remove)
             self.vs_info = None  # don't want to keep this around, since it has alignments against all the genes we removed (also maybe memory control)
             alremover = None  # memory control (not tested)
