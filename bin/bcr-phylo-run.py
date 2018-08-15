e0641193b8e6a1fe4191bcd51971ff1d48839c83
duncan ralph <dkralph@gmail.com>
2018-08-15 09:54:29 -0700

    add bcr phylo run script
    oops, that should've been committed before

---
+++ bin/bcr-phylo-run.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import os
+import sys
+# from ete3 import TreeNode, TreeStyle, NodeStyle, SVG_COLORS
+# import pickle
+
+current_script_dir = os.path.dirname(os.path.realpath(__file__)).replace('/bin', '/python')
+sys.path.insert(1, current_script_dir)
+import utils
+
+base_outdir = '_output'
+extrastr = 'xxx'
+
+# ----------------------------------------------------------------------------------------
+def simdir(stype):
+  return '%s/%s' % (base_outdir, stype)
+def plotdir(stype):
+  return '%s/partis/tmp/%s' % (os.getenv('fs'), stype)
+def simfname(stype):
+  return '%s/%s.fasta' % (simdir(stype), extrastr)
+
+# ----------------------------------------------------------------------------------------
+def simulate(stype):
+  bcr_phylo_path = os.getenv('PWD') + '/packages/bcr-phylo-benchmark'
+  ete_path = '/home/' + os.getenv('USER') + '/anaconda_ete/bin'
+
+  os.environ['TMPDIR'] = '/tmp'
+  cmd = 'export PATH=%s:$PATH && xvfb-run -a python %s/bin/simulator.py' % (ete_path, bcr_phylo_path)
+  cmd += ' --mutability %s/S5F/Mutability.csv --substitution %s/S5F/Substitution.csv' % (bcr_phylo_path, bcr_phylo_path)
+
+  if stype == 'neutral':
+      cmd += ' --lambda %f --lambda0 %f --N %d' % (1.5, 0.365, 1000)
+  elif stype == 'selection':
+      cmd += ' --selection'
+      cmd += ' --lambda %f --lambda0 %f' % (2., 0.365)
+      # cmd += ' --n %d' % 60
+      cmd += ' --T %d --target_dist 5 --target_count 100' % 100
+      cmd += ' --carry_cap 1000 --skip_update 100'
+  else:
+      assert False
+
+  cmd += ' --outbase %s/%s' % (simdir(stype), extrastr)
+  cmd += ' --random_seq %s/sequence_data/AbPair_naive_seqs.fa' % bcr_phylo_path
+  if not os.path.exists(simdir(stype)):
+      os.makedirs(simdir(stype))
+
+  utils.simplerun(cmd, shell=True, debug=True) #, dryrun=True)
+
+# # ----------------------------------------------------------------------------------------
+# def print_simulation(stype):
+#     with open('selection_sim_lineage_tree.p', 'rb') as fh:
+#         tree = pickle.load(fh)
+    
+#     # Print tree in ASCII:
+#     print(tree)
+#     # Root name and sequence:
+#     tree.name
+#     tree.sequence
+    
+#     # Traverse through the nodes and print their affinity (Kd):
+#     ### N.B. Smaller numberical value of affinity means higher fitness
+#     for node in tree.traverse():
+#         print(node.Kd)
+
+# ----------------------------------------------------------------------------------------
+def partition(stype):
+    # cmd = './bin/partis cache-parameters --infname %s --n-procs 8' % simfname(stype)
+    # utils.simplerun(cmd, debug=True, dryrun=True)
+    # cmd = './bin/partis partition --infname %s --plotdir %s --n-procs 8 --outfname %s/partition.csv' % (simfname(stype), plotdir(stype), simdir(stype))
+    # cmd = './bin/partis plot-partitions --plotdir %s --outfname %s/partition.csv' % (plotdir(stype), simdir(stype))
+    cmd = './bin/partis view-output --outfname %s/partition.csv --abb --parameter-dir _output/%s' % (simdir(stype), simfname(stype)[ : simfname(stype).rfind('.')].replace('/', '_'))
+    
+    utils.simplerun(cmd, debug=True) #, dryrun=True)
+
+for stype in ['selection', 'neutral']:
+    # simulate(stype)
+    partition(stype)
+    break
