1271ca7b934f9a5c042a214e2a88d5784f1f03af
duncan ralph <dkralph@gmail.com>
2018-08-19 15:16:49 -0700

    rearrange tree calculation script

--- bin/calculate_tree_metrics.py
+++ bin/calculate_tree_metrics.py
@@ -15,18 +15,22 @@ import utils
 import treeutils
 
 parser = argparse.ArgumentParser()
-parser.add_argument('treefile')
-parser.add_argument('--naive-seq-name', required=True)
+parser.add_argument('infile', help='input tree file name in newick format')
+parser.add_argument('outfile', help='output file name in yaml format')
+parser.add_argument('--naive-seq-name')
+parser.add_argument('--reroot-at-naive', action='store_true')
 args = parser.parse_args()
 
-tree = treeutils.get_bio_tree(treefname=args.treefile)
+if args.reroot_at_naive:
+    assert args.naive_seq_name is not None
+    print treeutils.get_ascii_tree(treeutils.get_treestr(args.infile))
+    dendro_tree = treeutils.get_dendro_tree(treefname=args.infile)
+    dendro_tree.reroot_at_node(dendro_tree.find_node_with_taxon_label(args.naive_seq_name), update_bipartitions=True)
+    # print dendro_tree.as_ascii_plot(width=100)  # why tf does this show them as all the same depth?
+    treestr = dendro_tree.as_string(schema='newick')  #, suppress_rooting=True)
+    print treeutils.get_ascii_tree(treestr)
+    bio_tree = treeutils.get_bio_tree(treestr=treestr)
+else:
+    bio_tree = treeutils.get_bio_tree(treefname=args.infile)
 
-treeutils.calculate_LBI(tree, debug=True)
-sys.exit()
-
-# print treeutils.get_ascii_tree(treeutils.get_treestr(args.treefile))
-# dendro_tree = treeutils.get_dendro_tree(treefname=args.treefile)
-# dendro_tree.reroot_at_node(dendro_tree.find_node_with_taxon_label(args.naive_seq_name), update_bipartitions=True)
-# # print dendro_tree.as_ascii_plot(width=100)  # why tf does this show them as all the same depth?
-# treestr = dendro_tree.as_string(schema='newick')  #, suppress_rooting=True)
-# print treeutils.get_ascii_tree(treestr)
+treeutils.calculate_LBI(bio_tree, debug=True)
