4c7b6a7fc4b88509d1fc9d1d33da93682367a39c
duncan ralph <dkralph@gmail.com>
2019-04-04 11:15:22 -0700

    add --only-print-seed-cluster

--- python/partitiondriver.py
+++ python/partitiondriver.py
@@ -367,7 +367,14 @@ class PartitionDriver(object):
 
     # ----------------------------------------------------------------------------------------
     def print_results(self, cpath, annotations):
+        seed_uid = None
+        if self.args.seed_unique_id is not None:
+            seed_uid = self.args.seed_unique_id
+
         if cpath is not None:
+            if seed_uid is not None and cpath.seed_unique_id != seed_uid:
+                print '  %s seed uids from args and cpath don\'t match %s %s ' % (utils.color('red', 'error'), self.args.seed_unique_id, cpath.seed_unique_id)
+            seed_uid = cpath.seed_unique_id
             print utils.color('green', 'partitions:')
             cpath.print_partitions(abbreviate=self.args.abbreviate, reco_info=self.reco_info, highlight_cluster_indices=self.args.cluster_indices, calc_missing_values=('all' if cpath.n_seqs() < 500 else 'best'))
             if not self.args.is_data and self.reco_info is not None:  # if we're reading existing output, it's pretty common to not have the reco info even when it's simulation, since you have to also pass in the simulation input file on the command line
@@ -384,13 +391,15 @@ class PartitionDriver(object):
             for line in sorted_annotations:
                 if self.args.only_print_best_partition and cpath is not None and cpath.i_best is not None and line['unique_ids'] not in cpath.partitions[cpath.i_best]:
                     continue
+                if self.args.only_print_seed_cluster and seed_uid not in line['unique_ids']:
+                    continue
                 label, post_label = '', ''
                 if self.args.infname is not None and self.reco_info is not None:
                     utils.print_true_events(self.simglfo, self.reco_info, line, extra_str='  ')
                     label = 'inferred:'
                 if cpath is not None and cpath.i_best is not None and line['unique_ids'] in cpath.partitions[cpath.i_best]:
                     post_label += ' (from best partition)'
-                utils.print_reco_event(line, extra_str='  ', label=label, post_label=post_label, seed_uid=self.args.seed_unique_id)
+                utils.print_reco_event(line, extra_str='  ', label=label, post_label=post_label, seed_uid=seed_uid)
 
     # ----------------------------------------------------------------------------------------
     def read_existing_output(self, outfname=None, ignore_args_dot_queries=False, read_partitions=False, read_annotations=False):
