13373b19fa0057dc5837dae627b68c5d7f8903f0
duncan ralph <dkralph@gmail.com>
2019-04-11 15:20:36 -0700

    add lb bound script

--- bin/plot-lb-bounds.py
+++ bin/plot-lb-bounds.py
@@ -2,38 +2,7 @@
 import argparse
 import os
 import sys
-
-# ----------------------------------------------------------------------------------------
-def plot_bounds(args):
-    for ntl in [5]:  #range(15):
-        # lbvals = treeutils.get_min_lbi(args.seq_len, args.lb_tau)
-        lbvals = treeutils.get_max_lbi(args.seq_len, args.lb_tau, n_tau_lengths=ntl)
-
-        assert args.plotdir is not None
-        import plotting
-        cmdfos = [plotting.get_lb_tree_cmd(lbvals['tree'], '%s/foo.svg' % args.plotdir, 'lbi', 'affinities', args.ete_path, args.workdir, metafo=lbvals)]
-        utils.run_cmds(cmdfos, clean_on_success=True, shell=True, debug='print')
-
-# ----------------------------------------------------------------------------------------
-parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('--lb-tau', required=True, type=float)
-parser.add_argument('--seq-len', default=400, type=int)
-parser.add_argument('--plotdir')
-parser.add_argument('--workdir')  # default set below
-parser.add_argument('--partis-dir', default=os.getcwd(), help='path to main partis install dir')
-parser.add_argument('--ete-path', default=('/home/%s/anaconda_ete/bin' % os.getenv('USER')) if os.getenv('USER') is not None else None)
-args = parser.parse_args()
-
-sys.path.insert(1, args.partis_dir + '/python')
-try:
-    import utils
-    import treeutils
-except ImportError as e:
-    print e
-    raise Exception('couldn\'t import from main partis dir \'%s\' (set with --partis-dir)' % args.partis_dir)
-
-if args.workdir is None:
-    args.workdir = utils.choose_random_subdir('/tmp/%s/hmms' % (os.getenv('USER', default='partis-work'))
+import yaml
 
 # import plotting
 # xvals, yvals = zip(*((1, 0.0034),
@@ -56,4 +25,62 @@ if args.workdir is None:
 # ax.plot(xvals, yvals)
 # plotting.mpl_finish(ax, XXX plotdir, 'foo')
 # sys.exit()
-plot_bounds(args)
+
+# ----------------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('--lb-tau-list', required=True)
+parser.add_argument('--n-tau-lengths-list', help='set either this or --n-generations-list')
+parser.add_argument('--n-generations-list', help='set either this or --n-tau-lengths-list')
+parser.add_argument('--seq-len', default=400, type=int)
+parser.add_argument('--outdir')
+parser.add_argument('--workdir')  # default set below
+parser.add_argument('--partis-dir', default=os.getcwd(), help='path to main partis install dir')
+parser.add_argument('--ete-path', default=('/home/%s/anaconda_ete/bin' % os.getenv('USER')) if os.getenv('USER') is not None else None)
+args = parser.parse_args()
+
+sys.path.insert(1, args.partis_dir + '/python')
+try:
+    import utils
+    import treeutils
+    import plotting
+except ImportError as e:
+    print e
+    raise Exception('couldn\'t import from main partis dir \'%s\' (set with --partis-dir)' % args.partis_dir)
+
+args.lb_tau_list = utils.get_arg_list(args.lb_tau_list, floatify=True)
+args.n_tau_lengths_list = utils.get_arg_list(args.n_tau_lengths_list, floatify=True)
+args.n_generations_list = utils.get_arg_list(args.n_generations_list, intify=True)
+if [args.n_tau_lengths_list, args.n_generations_list].count(None) != 1:
+    raise Exception('have to set exactly one of --n-tau-lengths, --n-generations')
+if args.n_tau_lengths_list is None:
+    args.n_tau_lengths_list = [None for _ in args.n_generations_list]
+if args.n_generations_list is None:
+    args.n_generations_list = [None for _ in args.n_tau_lengths_list]
+
+if args.workdir is None:
+    args.workdir = utils.choose_random_subdir('/tmp/%s/hmms' % (os.getenv('USER', default='partis-work')))
+
+# ----------------------------------------------------------------------------------------
+for lbt in args.lb_tau_list:
+    print '   lb tau %.4f' % lbt
+    if args.n_tau_lengths_list is not None:
+        print '         tau lengths'
+    elif args.n_generations_list is not None:
+        print '         generations'
+    for ntl, n_gen in zip(args.n_tau_lengths_list, args.n_generations_list):
+        print '           %s' % (('%.2f' % ntl) if ntl is not None else ('%d' % n_gen))
+        # lbvals = treeutils.get_min_lbi(args.seq_len, args. XXX lb_tau)
+        # max_name, max_lbi, lbvals = treeutils.get_max_lbi(args.seq_len, lbt, n_tau_lengths=ntl, n_generations=n_gen)
+
+# outdir=$fs/partis/lb-tau-optimization/test-2d/n_gen-$n_gen-lbt-$lbt
+	# limit_procs python
+    # if args.plotdir is not None:
+    #     utils.prep_dir(args.plotdir, wildlings='*.svg', allow_other_files=True)
+    #     cmdfos = [plotting.get_lb_tree_cmd(lbvals['tree'], '%s/tree.svg' % args.plotdir, 'lbi', 'affinities', args.ete_path, args.workdir, metafo=lbvals)]
+    #     utils.run_cmds(cmdfos, clean_on_success=True, shell=True, debug='print')
+
+    # if args.outfname is not None:
+    #     if not os.path.exists(os.path.dirname(args.outfname)):
+    #         os.makedirs(os.path.dirname(args.outfname))
+    #     with open(args.outfname, 'w') as outfile:
+    #         yaml.dump({'max' : {'name' : max_name, 'lbi' : max_lbi}})
