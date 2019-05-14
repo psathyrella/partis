1d6ca503413400cd3a80a67b859ceb2d19cb12c4
duncan ralph <dkralph@gmail.com>
2019-05-14 09:11:47 +0200

    update chimera-plot

--- bin/chimera-plot.py
+++ bin/chimera-plot.py
@@ -17,8 +17,11 @@ import glutils
 parser = argparse.ArgumentParser()
 parser.add_argument('infile')
 parser.add_argument('plotdir')
+parser.add_argument('--glfo-dir', default='data/germlines/human', help='I\'m hacking this in afterwards because this was written before switching to yaml output files, so I think it was using this default germline dir anyway (except it used old glfo with different genes, so you probably actually have to pass in the real corresponding glfo anyway)')
 parser.add_argument('--chunk-len', default=75, type=int)
+parser.add_argument('--cutoff', default=0.3, help='point in max-abs-diff above which we assume most sequences are chimeric')
 parser.add_argument('--title')
+parser.add_argument('--locus', default='igh')
 args = parser.parse_args()
 if args.title == 'good':
     args.title = 'none'
@@ -28,8 +31,10 @@ elif args.title == 'chimeras':
 def gk(uids):
     return ':'.join(uids)
 
-
-glfo, annotation_list, _ = utils.read_output(args.infile)
+glfo = None
+if utils.getsuffix(args.infile) == '.csv':
+    glfo = glutils.read_glfo(args.glfo_dir, args.locus)
+glfo, annotation_list, _ = utils.read_output(args.infile, glfo=glfo)
 annotations = collections.OrderedDict((line['unique_ids'][0], line) for line in annotation_list)
 
 chfo = {uid : {k : v for k, v in zip(('imax', 'max_abs_diff'), utils.get_chimera_max_abs_diff(annotations[uid], iseq=0, chunk_len=args.chunk_len))} for uid in annotations}
@@ -37,6 +42,10 @@ biggest_adiffs = sorted(chfo, key=lambda q: chfo[q]['max_abs_diff'], reverse=Tru
 for uid in biggest_adiffs[:5]:
     print '%-3d  %6.3f' % (chfo[uid]['imax'], chfo[uid]['max_abs_diff'])
     utils.print_reco_event(annotations[uid])
+
+n_above_cutoff = len([_ for cfo in chfo.values() if cfo['max_abs_diff'] > args.cutoff])
+chimeric_fraction = n_above_cutoff / float(len(chfo))
+print '  %d / %d = %.3f above chimeric cutoff' % (n_above_cutoff, len(chfo), chimeric_fraction)
 
 hmaxval = Hist(45, 0., 0.65)
 for uid in annotations:
