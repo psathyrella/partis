37cd89dd7299a24de03f40c1345eedd47f19717f
duncan ralph <dkralph@gmail.com>
2019-07-26 15:24:41 -0700

    update bcr-phylo

--- bin/run-tree-metrics.sh
+++ bin/run-tree-metrics.sh
@@ -19,5 +19,7 @@ testargs="--n-sim-seqs-per-gen-list 50:125 --lb-tau-list 0.002:0.003 --obs-times
 # echo $bin --label $label --n-replicates 10 --n-sim-events-per-proc 10 --only-csv-plots --carry-cap-list 1000 --obs-times-list 100:200:300:100,150:200,250:100,200,300 --n-sim-seqs-per-gen-list 100:100:100:50:50:33 --zip-vars obs-times:n-sim-seqs-per-gen
 # label=vary-obs-times-v1
 # echo $bin --label $label --n-replicates 10 --n-sim-events-per-proc 10 --only-csv-plots --carry-cap-list 1000 --obs-times-list 300:100,200,300:200,250,300 --n-sim-seqs-per-gen-list 100:33:33 --zip-vars obs-times:n-sim-seqs-per-gen
-label=vary-obs-frac-v0
-echo $bin --label $label --n-replicates 30 --n-sim-events-per-proc 10 --only-csv-plots --carry-cap-list 1000 --obs-times-list 150 --n-sim-seqs-per-gen-list 30:50:75:100:150:200
+# label=vary-obs-frac-v0
+# echo $bin --label $label --n-replicates 30 --n-sim-events-per-proc 10 --only-csv-plots --carry-cap-list 1000 --obs-times-list 150 --n-sim-seqs-per-gen-list 30:50:75:100:150:200
+label=vary-metric-v0
+echo $bin --label $label --n-replicates 30 --n-sim-events-per-proc 10 --only-csv-plots --carry-cap-list 1000 --obs-times-list 150 --n-sim-seqs-per-gen-list 100 --metric-for-target-distance-list nuc:aa:aa-sim-ascii:aa-sim-blosum
