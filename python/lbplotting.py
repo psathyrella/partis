0cd818eb2e035c7f66addaef811cdb318a2e13fc
duncan ralph <dkralph@gmail.com>
2019-10-28 09:22:38 -0700

    plotting tweaks

--- python/lbplotting.py
+++ python/lbplotting.py
@@ -312,7 +312,7 @@ def make_lb_affinity_joyplots(plotdir, lines, lb_metric):
     annotation_dict = {':'.join(l['unique_ids']) : l for l in lines}
     sorted_clusters = sorted(partition, key=lambda c: numpy.mean(annotation_dict[':'.join(c)]['affinities']), reverse=True)
     sorted_clusters = sorted(sorted_clusters, key=lambda c: max(annotation_dict[':'.join(c)]['affinities']), reverse=True)  # ends up sorted by max(), with ties broken by mean()
-    partplotter.make_single_joyplot(sorted_clusters, annotation_dict, sum([len(c) for c in sorted_clusters]), plotdir, '%s-affinity-joyplot' % lb_metric, x1key='affinities', x1label='affinity', x2key=lb_metric)
+    partplotter.make_single_joyplot(sorted_clusters, annotation_dict, sum([len(c) for c in sorted_clusters]), plotdir, '%s-affinity-joyplot' % lb_metric, x1key='affinities', x1label='affinity', x2key=lb_metric, x2label=lb_metric)
 
 # ----------------------------------------------------------------------------------------
 def plot_2d_scatter(plotname, plotdir, plotvals, yvar, ylabel, title, xvar='affinity', xlabel='affinity', log='', leg_loc=None, warn_text=None):
