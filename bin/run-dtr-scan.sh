5e089c98173f03155b511dcdb52905e9553eca10
duncan ralph <dkralph@gmail.com>
2019-12-26 15:09:38 -0800

    add run-dtr-scan.sh

---
+++ bin/run-dtr-scan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+action=test  # plot
+trlabel=dtr-train-v1; trseed=0
+# trlabel=dtr-train-v2; trseed=0 #1  # NOTE dtr-scan.py doesn't actually check what seed was used for training, it relies on us to pass in the correct one here
+common=" --training-label $trlabel --training-seed $trseed --n-max-queries 250000" # --cgroup among-families --tvar affinity"
+# ./test/dtr-scan.py $action --label dtr-train-v0 $common
+# ./test/dtr-scan.py $action --label dtr-train-v1 $common
+echo ./test/dtr-scan.py $action --label dtr-train-v2 $common
