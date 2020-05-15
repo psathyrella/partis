f9b2553ee2d88a80b61308e3b9c95d8dc085e6d9
duncan ralph <dkralph@gmail.com>
2020-05-14 17:31:15 -0700

    fix missing bin/bash

--- packages/bpp-newlik/install.sh
+++ packages/bpp-newlik/install.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 # this should install the newlik branch of bpp, which you need to run simulation with --per-base-mutation set
 
 bd=$PWD #/fh/fast/matsen_e/dralph/code/partis-dev/packages/bpp-src
+clone="false"  # if this is false, we assume the source for each subpackage is already here because git submodule was already run; if this is true, we clone each subpackage and then checkout the proper branch (which shouldn't need to happen any more unless i want to update versions or something)
 
 cd $bd
 
@@ -12,30 +15,44 @@ done
 for pack in $packs; do
     echo -e "\n$pack"
 
-    # clone repo
     if [ $pack == "eigen3" ]; then
     	repo=eigenteam/eigen-git-mirror
     else
     	repo=BioPP/$pack
     fi
-    git clone git@github.com:$repo $pack
 
-    cd $pack
-
-    # switch branches
-    if [ $pack != "eigen3" ]; then  # crashes on one of the bio repos, which doesn't have a newlik branch
-    	branch=newlik # devel
-    	git branch --track $branch origin/$branch
-    	git checkout $branch
+    if [ "$clone" == "true" ]; then
+	git clone git@github.com:$repo $pack
     fi
 
-    # TODO install patch here
+    cd $pack
+    if ! [ $? -eq 0 ]; then  # don't want to keep going if the dir isn't there
+	echo "cd $pack failed"
+	exit 1
+    fi
+
+    if [ "$clone" == "true" ]; then
+	# switch branches
+	if [ $pack != "eigen3" ]; then  # crashes on one of the bio repos, which doesn't have a newlik branch
+    	    branch=newlik # devel
+    	    git branch --track $branch origin/$branch
+    	    git checkout $branch
+	fi
+    fi
+
+    if [ -f ../$pack.patch ]; then  # the version i checkout out was missing a couple includes
+	git apply --verbose ../$pack.patch
+    fi
 
     # configure/compile
     mkdir -p _build
     cd _build
     cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=$bd/_build ..  # -DCMAKE_PREFIX_PATH=/home/dralph/work/partis/packages/bpp-src/bpp-core
     make install
+    if ! [ $? -eq 0 ]; then  # don't want to keep going if one compile failed
+	echo "$pack installation failed"
+	exit 1
+    fi
     cd ..
 
     # git status
