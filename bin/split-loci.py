3fcdf1c702617e27cda4ac8ca9476735b8f0fee8
duncan ralph <dkralph@gmail.com>
2020-09-02 10:29:37 -0700

    better split-loci.py output behavior

--- bin/split-loci.py
+++ bin/split-loci.py
@@ -17,18 +17,19 @@ from clusterpath import ClusterPath
 
 dstr = """
 use vsearch to split the sequences in <fname> according to their ig loci, either writing each locus to its own fasta file <fname>-<locus>.fa (by default), 
-or additionally splitting the igh sequences according to the light chain locus with which they\'re paired, thus resulting in two directories igh+igk/ and igh+igl/ (if --paired-output-dirs is set).
+or additionally splitting the igh sequences according to the light chain locus with which they\'re paired, thus resulting in two directories igh+igk/ and igh+igl/ (if --split-heavy-seqs is set).
 """
 parser = argparse.ArgumentParser(description=dstr,
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)  # why tf isn't this printing the defaults?
 parser.add_argument('fname', help='fasta input file')
+parser.add_argument('--outdir', help='output directory (if not set, output is written to directory of <fname>)')
 parser.add_argument('--species', default='human', choices=('human', 'macaque', 'mouse'), help='Which species?')
 parser.add_argument('--germline-dir', default=partis_dir + '/data/germlines', help='doesn\'t need to be the germlines corresponding to this sample since it\'s just so it can figure out which is igh vs igk vs igl, so the default is probably fine')
 parser.add_argument('--workdir', default=utils.choose_random_subdir('/tmp/%s/partis' % os.getenv('USER', default='partis-work')), help='working directory for vsearch')
 parser.add_argument('--vsearch-binary', help='Path to vsearch binary (vsearch binaries for linux and darwin are included in partis/bin/, so leaving this unset should work, but for other systems you need to get your own)')
 parser.add_argument('--debug', action='store_true')
 parser.add_argument('--fasta-info-index', type=int, help='zero-based index in fasta info of sequence name string (e.g. if name line is \'>stuff more-stuff NAME extra-stuff\' the index should be 2)')
-parser.add_argument('--paired-output-dirs', action='store_true', help='instead of the default of writing each locus to its own .fa file, split igh into separate dirs for those sequences paired with igk and igl (i.e. so each of the two dirs is ready for partis --paired-loci)')
+parser.add_argument('--split-heavy-seqs', action='store_true', help='instead of the default of writing each locus to its own .fa file, split igh into separate dirs for those sequences paired with igk and igl (i.e. so each of the two dirs is ready for partis --paired-loci)')
 args = parser.parse_args()
 
 seqfos = utils.read_fastx(args.fname)
@@ -71,24 +72,30 @@ print 'totals: %s' % ' '.join(('%s %d'%(l, len(sfos))) for l, sfos in outfos.ite
 # ----------------------------------------------------------------------------------------
 def write_locus_file(ofn, ofos):
     print '    %s: %d to %s' % (locus, len(outfos[locus]), ofn)
+    if not os.path.exists(os.path.dirname(ofn)):
+        os.makedirs(os.path.dirname(ofn))
     with open(ofn, 'w') as lfile:
         for sfo in outfos[locus]:
             lfile.write('>%s\n%s\n' % (sfo['name'], sfo['seq']))
 
 # ----------------------------------------------------------------------------------------
 print 'writing:'
-if args.paired_output_dirs:
+if args.outdir is None:
+    outbase = utils.getprefix(args.fname)  # if --outdir wasn't set, we include basename of input file, since they might have multiple files in the same dir
+    tmp_sep = '-'
+else:
+    outbase = args.outdir  # but if they did set --outdir, it's nicer to have plain locus file/path names
+    tmp_sep = '/'
+if args.split_heavy_seqs:
     for l_locus in l_loci:
         l_uids = set(sfo['name'] for sfo in outfos[l_locus])
         h_outfo = [sfo for sfo in outfos[h_locus] if sfo['name'] in l_uids]  # heavy chain seqs corresponding to this light chain
-        ldir = '%s-%s+%s' % (utils.getprefix(args.fname), h_locus, l_locus)
+        ldir = '%s%s%s+%s' % (outbase, tmp_sep, h_locus, l_locus)
         print '  %d/%d %s seqs pair with %s' % (len(h_outfo), len(outfos[h_locus]), h_locus, l_locus)
-        if not os.path.exists(ldir):
-            os.makedirs(ldir)
         write_locus_file('%s/%s.fa' % (ldir, h_locus), h_outfo)
         write_locus_file('%s/%s.fa' % (ldir, l_locus), outfos[l_locus])
 else:
     for locus in outfos:
         if len(outfos[locus]) == 0:
             continue
-        write_locus_file('%s-%s.fa' % (utils.getprefix(args.fname), locus), outfos[locus])
+        write_locus_file('%s%s%s.fa' % (outbase, tmp_sep, locus), outfos[locus])
