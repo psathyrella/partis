c39507bddc68e1facc75a17c5fe8b6a56acf5ca3
duncan ralph <dkralph@gmail.com>
2020-09-15 09:46:33 -0700

    handle multiple overlapping clusters in add-seqs-to-outputs.py
    and add --partition-index

--- bin/add-seqs-to-outputs.py
+++ bin/add-seqs-to-outputs.py
@@ -25,6 +25,7 @@ parser = argparse.ArgumentParser(description=dstr,
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)  # why tf isn't this printing the defaults?
 parser.add_argument('--new-seq-file', required=True, help='fasta input file with seqs to be added to annotations + partitions in partis output yaml')
 parser.add_argument('--partis-output-file', required=True, help='partis output file to which to add the seqs from --new-seq-file')
+parser.add_argument('--partition-index', type=int, help='index of partition from which to take the clusters/annotations (if not set, uses the best partition)')
 parser.add_argument('--glfo-dir', default=partis_dir + '/data/germlines/human', help='germline info directory. Only used if --partis-output-file is an old-style .csv, and this default dir may work if your output file doesn\'t have novel inferred genes. Otherwise, is the germline info dir from the partis inferred parameter directory corresponding to your output file --partis-output-file.')
 parser.add_argument('--locus', default='igh')
 parser.add_argument('--outfile', required=True, help='output partis yaml file')
@@ -41,17 +42,26 @@ if utils.getsuffix(args.partis_output_file) == '.csv':
     glfo = glutils.read_glfo(args.glfo_dir, locus=args.locus)
 
 glfo, annotation_list, cpath = utils.read_output(args.partis_output_file, glfo=glfo, locus=args.locus)
-
-partition = cpath.partitions[cpath.i_best]
+if args.partition_index is not None:
+    print '  using non-best partition index %d (best is %d)' % (args.partition_index, cpath.i_best)
+partition = cpath.partitions[cpath.i_best if args.partition_index is None else args.partition_index]
 print '    read partition with %d clusters from %s' % (len(partition), args.partis_output_file)
 
 new_uids = set(sfo['name'] for sfo in new_seqfos)
-clusters_with_overlap = [c for c in partition if len(set(c) & new_uids) > 0]
+clusters_with_overlap = []
+for cluster in partition:
+    overlap_uids = set(cluster) & new_uids
+    if len(overlap_uids) > 0:
+        clusters_with_overlap.append((cluster, overlap_uids))
+
 if len(clusters_with_overlap) == 0:
     raise Exception('no clusters in partition have any overlap with sequences from fasta file')
 elif len(clusters_with_overlap) > 1:
-    raise Exception('too many clusters %d in the partition overlaps with sequences from the fasta file' % len(clusters_with_overlap))
-old_cluster = clusters_with_overlap[0]
+    # raise Exception('too many clusters %d in the partition overlaps with sequences from the fasta file' % len(clusters_with_overlap))
+    clusters_with_overlap = sorted(clusters_with_overlap, key=lambda p: len(p[1]), reverse=True)
+    ostrs = ['%d %d'%(len(c), len(o)) for c, o in clusters_with_overlap]
+    print '  %s more than one cluster overlaps with sequences from fasta file, just taking first one (size overlap): %s,  %s' % (utils.color('yellow', 'warning'), utils.color('red', ostrs[0]), ',  '.join(ostrs[1:]))
+old_cluster = clusters_with_overlap[0][0]
 
 print '    adding %d fasta sequences to cluster of size %d (%d fasta sequences were already in cluster)' % (len(new_uids - set(old_cluster)), len(old_cluster), len(new_uids & set(old_cluster)))
 sfos_to_add = [sfo for sfo in new_seqfos if sfo['name'] not in old_cluster]
