42a95d96ea865d7513b1fdd182b489223e757a7e
duncan ralph <dkralph@gmail.com>
2020-10-23 11:52:24 -0700

    add --input-metafname to bin/split-loci.py

--- bin/split-loci.py
+++ bin/split-loci.py
@@ -14,10 +14,12 @@ sys.path.insert(1, partis_dir + '/python')
 import utils
 import glutils
 from clusterpath import ClusterPath
+import seqfileopener
 
 dstr = """
 use vsearch to split the sequences in <fname> according to their ig loci, writing each locus to its own fasta file <fname>-<locus>.fa.
 If --split-heavy-seqs is set, also splits the igh sequences according to the light chain locus with which they\'re paired, thus resulting in two directories igh+igk/ and igh+igl/.
+Pairing information can be specified either by specifying --input-metafname (see main partis help) with the key \'paired-uids\' a list of the sequences with which each uid is paired, or by simply giving each pair of sequences the same uid (the latter does *not* support multiple pairings).
 Use --reverse-negative-strands to check both senses for each input sequence.
 """
 parser = argparse.ArgumentParser(description=dstr,
@@ -30,9 +32,11 @@ parser.add_argument('--germline-dir', default=partis_dir + '/data/germlines', he
 parser.add_argument('--workdir', default=utils.choose_random_subdir('/tmp/%s/partis' % os.getenv('USER', default='partis-work')), help='working directory for vsearch')
 parser.add_argument('--vsearch-binary', help='Path to vsearch binary (vsearch binaries for linux and darwin are included in partis/bin/, so leaving this unset should work, but for other systems you need to get your own)')
 parser.add_argument('--vsearch-threshold', default=0.4, help='default identity threshold for vsearch')
-parser.add_argument('--debug', action='store_true')
+parser.add_argument('--debug', action='store_true', default=True)
 parser.add_argument('--overwrite', action='store_true')
 parser.add_argument('--fasta-info-index', type=int, help='zero-based index in fasta info/meta string of sequence name/uid (e.g. if name line is \'>stuff more-stuff NAME extra-stuff\' the index should be 2)')
+parser.add_argument('--input-metafname', help='yaml file with meta information keyed by sequence id. See same argument in main partis help, and https://github.com/psathyrella/partis/blob/master/docs/subcommands.md#input-meta-info for an example.')
+parser.add_argument('--n-max-queries', type=int)
 parser.add_argument('--split-heavy-seqs', action='store_true', help='In addition to writing a fasta file for each locus with every sequence from that locus, also split the igh sequences into separate subdirs for those sequences paired with igk and those paired with igl (so each of these two subdirs is ready for partis --paired-loci)')
 parser.add_argument('--ig-or-tr', default='ig', choices=utils.locus_pairs.keys(), help='antibodies or TCRs?')
 args = parser.parse_args()
@@ -52,7 +56,7 @@ def use_rev_comp(pline, rline):  # decide whether positive sense <pline> or nega
 # ----------------------------------------------------------------------------------------
 if os.path.dirname(args.fname) == '':
     args.fname = '%s/%s' % (os.getcwd(), args.fname)
-seqfos = utils.read_fastx(args.fname)
+seqfos = utils.read_fastx(args.fname, n_max_queries=args.n_max_queries)
 if args.fasta_info_index is not None:
     for sfo in seqfos:
         sfo['name'] = sfo['infostrs'][args.fasta_info_index]
@@ -94,7 +98,7 @@ for sfo in seqfos:
     if max_score == 0:
         failed_seqs.append(sfo)
     outfos[locus].append(sfo)
-    if args.debug:
+    if args.debug > 1:
         def lpstr(spair):
             l, s = spair
             return '%s %d' % (utils.color('blue' if l==locus else None, l), s)
@@ -109,7 +113,7 @@ def write_locus_file(locus, ofn, ofos, extra_str='  '):
         return
     if utils.output_exists(args, ofn, offset=4):
         return
-    print '%s%s: %d to %s' % (extra_str, locus, len(ofos), ofn)
+    print '%s%s: %d to %s' % (extra_str, locus, len(ofos), os.path.basename(ofn))
     if not os.path.exists(os.path.dirname(ofn)):
         os.makedirs(os.path.dirname(ofn))
     with open(ofn, 'w') as lfile:
@@ -117,23 +121,59 @@ def write_locus_file(locus, ofn, ofos, extra_str='  '):
             lfile.write('>%s\n%s\n' % (sfo['name'], sfo['seq']))
 
 # ----------------------------------------------------------------------------------------
-print 'writing:'
 if args.outdir is None:
     outbase = utils.getprefix(args.fname)  # if --outdir wasn't set, we include basename of input file, since they might have multiple files in the same dir
     tmp_sep = '-'
 else:
     outbase = args.outdir  # but if they did set --outdir, it's nicer to have plain locus file/path names
     tmp_sep = '/'
+print 'writing to %s/' % (args.outdir if args.outdir is not None else os.getcwd())
 if len(failed_seqs) > 0:
     write_locus_file('failed', '%s%s%s.fa' % (outbase, tmp_sep, 'failed'), failed_seqs)
+
 for locus in outfos:  # first write the single files with all seqs for each locus
     write_locus_file(locus, '%s%s%s.fa' % (outbase, tmp_sep, locus), outfos[locus])
+
+paired_uids = {}
+if args.input_metafname is not None:  # we only really need this info now for the dbg printing below, since i think it doesn't really make sense to split into the paired subdirs anymore
+    for locus in tmploci:
+        dummy_annotation_list = [{'unique_ids' : [sfo['name']]} for sfo in outfos[locus]]
+        seqfileopener.read_input_metafo(args.input_metafname, dummy_annotation_list, required_keys=['paired-uids'])
+        paired_uids[locus] = {l['unique_ids'][0] : l['paired-uids'][0] for l in dummy_annotation_list}
+    if args.debug:  # this is kind of weird to have here, but it's just to print the info of who was paired with who after we know what chain everyone is. We don't really do anything with it here tho
+        print '  read pairing info for %d seqs from input meta file' % sum(len(pids) for pids in paired_uids.values())
+        loci_by_uid = {sfo['name'] : l for l in outfos for sfo in outfos[l]}  # locus of each sequence, just for counting below
+        print_cutoff = 0.01
+        print '            count  frac  paired with'
+        def locstr(l): return utils.color({'igh' : 'blue', 'igk' : 'purple', 'igl' : 'green'}.get(l, None), l.replace('ig', ''))
+        for locus in tmploci:
+            plocicounts = {}
+            for sfo in outfos[locus]:
+                plstr = ' '.join(locstr(l) for l in sorted([loci_by_uid.get(pid, '-') for pid in paired_uids[locus][sfo['name']]]))
+                if plstr not in plocicounts:
+                    plocicounts[plstr] = 0
+                plocicounts[plstr] += 1
+            total = sum(plocicounts.values())
+            n_skipped = 0
+            for ipl, (plstr, counts) in enumerate(sorted(plocicounts.items(), key=operator.itemgetter(1), reverse=True)):
+                if counts / float(total) < print_cutoff:
+                    n_skipped += counts
+                    continue
+                print '       %s  %6d  %5.2f   %s' % (locstr(locus) if ipl==0 else ' ', counts, counts / float(total), plstr)
+            if n_skipped > 0:
+                print '                +%d counts skipped with <%.3f each' % (n_skipped , print_cutoff) # utils.color('yellow', 'note
+
+# TODO i don't know that it even makes sense to split into subdirs like this if we don't know for sure who pairs with who. Like then we have two different heavy partitions in the subdirs
 if args.split_heavy_seqs:  # then, if necessary, write the ones that're split by pairing
     print '  writing to paired subdirs'
-# TODO handle seqs paired with multiple others somehow
     for h_locus, l_locus in utils.locus_pairs[args.ig_or_tr]:
-        l_uids = set(sfo['name'] for sfo in outfos[l_locus])
-        h_outfo = [sfo for sfo in outfos[h_locus] if sfo['name'] in l_uids]  # heavy chain seqs corresponding to this light chain
+        if args.input_metafname is not None:  # get heavy chain seqs corresponding to this light chain
+            all_paired_uids = set(u for pids in paired_uids[l_locus].values() for u in pids)  # all uids that are paired with any <l_locus> uid (note that in general not all of these are heavy chain)
+            h_outfo = [sfo for sfo in outfos[h_locus] if sfo['name'] in all_paired_uids]  # any h seq that was in a droplet with any of <l_uids>
+        else:
+            l_uids = set(sfo['name'] for sfo in outfos[l_locus])
+            h_outfo = [sfo for sfo in outfos[h_locus] if sfo['name'] in l_uids]  # if no meta file specified, just pair up the ones with identical uids
+
         ldir = '%s%s%s+%s' % (outbase, tmp_sep, h_locus, l_locus)
         print '      %d/%d %s seqs pair with %s' % (len(h_outfo), len(outfos[h_locus]), h_locus, l_locus)
         if len(h_outfo) == 0:
