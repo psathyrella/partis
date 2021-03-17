7e4c90b984b6837e897c84ea1995ee0bf3b17e32
duncan ralph <dkralph@gmail.com>
2021-03-17 14:36:14 -0700

    add run-paired-loci.sh

---
+++ bin/run-paired-loci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bin=./test/cf-paired-loci.py
+
+# label=v
+# testlabel=test-old
+# testargs="--n-sim-seqs-per-gen-list 50:125 --lb-tau-list 0.002:0.003 --obs-times 100 --carry-cap 1000 --n-generations-list 4:5"
