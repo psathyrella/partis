1106f4ad23b13e5529937d86b6d1e2b86e43b214
duncan ralph <dkralph@gmail.com>
2021-10-07 08:14:52 -0700

    update run-paired-loci.sh

--- bin/run-paired-loci.sh
+++ bin/run-paired-loci.sh
@@ -2,15 +2,12 @@
 
 bin=./test/cf-paired-loci.py
 
+methods=partition:vsearch-partition:vjcdr3-0.9:synth-distance-0.03:synth-singletons-0.20
+astr="--actions simu:cache-parameters:$methods"
+# astr="--actions plot --plot-metrics $methods"
+common="--n-sub-procs 7 --n-max-procs 5 --single-light-locus igk --base-outdir /fh/local/dralph/partis/paired-loci $astr --dry"
 for act in simulate cache-parameters partition; do
-    actstr="--actions simulate"
-
-    largs="--label n-final-test"
-    echo $bin $largs $actstr --version v1 --n-sim-events 10  --n-leaf-list 30  --allowed-cdr3-lengths 33:36:42:45 --mutation-multiplier 7
-    $bin $largs $actstr --version v2 --n-sim-events 100 --n-leaf-list 30  --allowed-cdr3-lengths 33:36:42:45 --mutation-multiplier 7
-    $bin $largs $actstr --version v3 --n-sim-events 3   --n-leaf-list 300 --allowed-cdr3-lengths 33:36:42:45 --mutation-multiplier 7
-    $bin $largs $actstr --version v4 --n-sim-events 10  --n-leaf-list 30  --allowed-cdr3-lengths 33:36:42:45 --mutation-multiplier 7 --cells-per-drop-list 1
+    $bin --label vs-shm          --version v0 --n-replicates 5 --n-leaves-list 5:10 --n-sim-events-list 1000 --scratch-mute-freq-list 0.01:0.05:0.10:0.30:0.50:0.80 --final-plot-xvar scratch-mute-freq --x-legend-var mfreq --combo-extra-str scratch-x-var --pvks-to-plot 10 $common
+    $bin --label vs-n-leaves     --version v0 --n-replicates 3 --n-leaves-list 1:5:10:50 --n-sim-events-list 1000 --simu-extra-args="--constant-number-of-leaves" $common
+    $bin --label vs-n-sim-events --version v0 --n-replicates 3 --n-leaves-list 1 --n-sim-events-list 100:1000:10000:50000 --simu-extra-args="--constant-number-of-leaves" $common
 done
-./test/cf-paired-loci.py --n-leaves-list 5:10 --label vs-shm --version v0 --n-replicates 5 --n-sim-events-list 1000 --scratch-mute-freq-list 0.01:0.05:0.10:0.30:0.50:0.80 --n-sub-procs 7 --n-max-procs 5 --single-light-locus igk --base-outdir /fh/local/dralph/partis/paired-loci --actions cache-parameters:partition:vsearch-partition:vjcdr3-0.9:synth-distance-0.03:synth-singletons-0.20 --dry  --final-plot-xvar scratch-mute-freq --combo-extra-str scratch-x-var --pvks-to-plot 10
-./test/cf-paired-loci.py --label vs-n-leaves --version v0 --n-replicates 3 --n-leaves-list 1:5:10:50 --simu-extra-args="--constant-number-of-leaves" --n-sim-events-list 1000 --n-sub-procs 5 --n-max-procs 5 --single-light-locus igk --base-outdir /fh/local/dralph/partis/paired-loci --actions simu:cache-parameters:partition:vsearch-partition:vjcdr3-0.9:synth-distance-0.03:synth-singletons-0.20 --dry
-# --x-legend-var mfreq
