5a7c06f2aaab46aa89ce008c5c85cd7fb9328c8b
duncan ralph <dkralph@gmail.com>
2021-10-08 08:29:50 -0700

    update run-paired-loci.sh

--- bin/run-paired-loci.sh
+++ bin/run-paired-loci.sh
@@ -5,9 +5,11 @@ bin=./test/cf-paired-loci.py
 methods=partition:vsearch-partition:vjcdr3-0.9:synth-distance-0.03:synth-singletons-0.20
 astr="--actions simu:cache-parameters:$methods"
 # astr="--actions plot --plot-metrics $methods"
-common="--n-sub-procs 7 --n-max-procs 5 --single-light-locus igk --base-outdir /fh/local/dralph/partis/paired-loci $astr --dry"
-for act in simulate cache-parameters partition; do
-    $bin --label vs-shm          --version v0 --n-replicates 5 --n-leaves-list 5:10 --n-sim-events-list 1000 --scratch-mute-freq-list 0.01:0.05:0.10:0.30:0.50:0.80 --final-plot-xvar scratch-mute-freq --x-legend-var mfreq --combo-extra-str scratch-x-var --pvks-to-plot 10 $common
-    $bin --label vs-n-leaves     --version v0 --n-replicates 3 --n-leaves-list 1:5:10:50 --n-sim-events-list 1000 --simu-extra-args="--constant-number-of-leaves" $common
-    $bin --label vs-n-sim-events --version v0 --n-replicates 3 --n-leaves-list 1 --n-sim-events-list 100:1000:10000:50000 --simu-extra-args="--constant-number-of-leaves" $common
-done
+# astr="--actions combine-plots --plot-metrics $methods"
+common="--n-sub-procs 15 --n-max-procs 5 --single-light-locus igk --base-outdir /fh/local/dralph/partis/paired-loci $astr --dry"
+# echo $bin --label vs-shm          --version v0 --n-replicates 5 --n-leaves-list 5:10 --n-sim-events-list 1000 --scratch-mute-freq-list 0.01:0.05:0.10:0.30:0.50:0.80 --final-plot-xvar scratch-mute-freq --x-legend-var mfreq --combo-extra-str scratch-x-var --pvks-to-plot 10 $common
+# echo $bin --label vs-shm          --version v1 --n-replicates 3 --n-leaves-list 10 --n-sim-events-list 1000 --scratch-mute-freq-list 0.01:0.05:0.10:0.30 --simu-extra-args=\"--flat-mute-freq --same-mute-freq-for-all-seqs\" --final-plot-xvar scratch-mute-freq $common  # with these simu args, the scratch mute freq is almost identical to the final mean mfreq, so can just use the scratch mute freq on x axis
+echo $bin --label vs-shm          --version v2 --n-replicates 3 --n-leaves-list 3 --n-sim-events-list 10000 --scratch-mute-freq-list 0.01:0.05:0.10:0.30 --simu-extra-args=\"--flat-mute-freq --same-mute-freq-for-all-seqs\" --final-plot-xvar scratch-mute-freq $common  # with these simu args, the scratch mute freq is almost identical to the final mean mfreq, so can just use the scratch mute freq on x axis
+# echo $bin --label vs-n-leaves     --version v0 --n-replicates 3 --n-leaves-list 1:5:10:50 --n-sim-events-list 1000 --simu-extra-args="--constant-number-of-leaves" $common
+# echo $bin --label vs-n-sim-events --version v0 --n-replicates 3 --n-leaves-list 1 --n-sim-events-list 100:1000:10000:50000 --simu-extra-args=\"--constant-number-of-leaves\" $common
+# echo $bin --label vs-n-sim-events --version v1 --n-replicates 3 --n-leaves-list 1 --n-sim-events-list 100:1000:10000:50000 --scratch-mute-freq-list 0.07:0.15 --simu-extra-args=\"--flat-mute-freq --same-mute-freq-for-all-seqs --constant-number-of-leaves\" $common
