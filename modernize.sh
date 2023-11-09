cc1cabb74690f6efce12cbd1231f0b7a945048fc
duncan ralph <dkralph@gmail.com>
2023-11-09 07:03:26 -0800

    run file fixer

--- modernize.sh
+++ modernize.sh
@@ -17,9 +17,11 @@ libmodernize.fixes.fix_filter \
 libmodernize.fixes.fix_zip \
 libmodernize.fixes.fix_xrange_six \
 libmodernize.fixes.fix_next \
-libmodernize.fixes.fix_print
+libmodernize.fixes.fix_print \
+libmodernize.fixes.fix_input_six \
+libmodernize.fixes.fix_file
 "
-fixers=libmodernize.fixes.fix_print
+fixers=libmodernize.fixes.fix_file
 paths="python/*.py test bin/*.py datascripts/*.py projects/*.py"
 for fixer in $fixers; do
     modernize -wnf $fixer $paths
