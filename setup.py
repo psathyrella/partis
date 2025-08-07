3a0ab4d65c6e0d2583dc330b4c6b1d3677d2a53f
duncan ralph <dkralph@gmail.com>
2025-08-07 15:16:23 -0700

    fix scons, increment version

--- setup.py
+++ setup.py
@@ -171,7 +171,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # Version
-version = '1.0.0'
+version = '1.0.3'
 
 setup(
     name='partis-bcr',
